0a25c13bfb33b56472d60de384a317c2a98ee839
Ярослав <yaroslaw74@gmail.com>
2025-12-21 20:45:21 +0200

    Update stimulus_bootstrap.js

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,5 +1,7 @@
 import { startStimulusApp } from "@symfony/stimulus-bundle";
+import Swup from "@symfony/ux-swup";
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+app.register("swup", Swup);
