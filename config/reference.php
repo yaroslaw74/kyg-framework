1b2facb33c86207c2b0db060cb0ab342cc790cba
Ярослав <yaroslaw74@gmail.com>
2025-12-27 16:58:45 +0200

    Update reference.php

--- config/reference.php
+++ config/reference.php
@@ -1618,6 +1618,60 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *     },
  *     embed_sourcemap?: bool|null, // Deprecated: Option "embed_sourcemap" at "symfonycasts_sass.embed_sourcemap" is deprecated. Use "sass_options.embed_source_map" instead". // Default: null
  * }
+ * @psalm-type StofDoctrineExtensionsConfig = array{
+ *     orm?: array<string, array{ // Default: []
+ *         translatable?: scalar|null, // Default: false
+ *         timestampable?: scalar|null, // Default: false
+ *         blameable?: scalar|null, // Default: false
+ *         sluggable?: scalar|null, // Default: false
+ *         tree?: scalar|null, // Default: false
+ *         loggable?: scalar|null, // Default: false
+ *         ip_traceable?: scalar|null, // Default: false
+ *         sortable?: scalar|null, // Default: false
+ *         softdeleteable?: scalar|null, // Default: false
+ *         uploadable?: scalar|null, // Default: false
+ *         reference_integrity?: scalar|null, // Default: false
+ *     }>,
+ *     mongodb?: array<string, array{ // Default: []
+ *         translatable?: scalar|null, // Default: false
+ *         timestampable?: scalar|null, // Default: false
+ *         blameable?: scalar|null, // Default: false
+ *         sluggable?: scalar|null, // Default: false
+ *         tree?: scalar|null, // Default: false
+ *         loggable?: scalar|null, // Default: false
+ *         ip_traceable?: scalar|null, // Default: false
+ *         sortable?: scalar|null, // Default: false
+ *         softdeleteable?: scalar|null, // Default: false
+ *         uploadable?: scalar|null, // Default: false
+ *         reference_integrity?: scalar|null, // Default: false
+ *     }>,
+ *     class?: array{
+ *         translatable?: scalar|null, // Default: "Gedmo\\Translatable\\TranslatableListener"
+ *         timestampable?: scalar|null, // Default: "Gedmo\\Timestampable\\TimestampableListener"
+ *         blameable?: scalar|null, // Default: "Gedmo\\Blameable\\BlameableListener"
+ *         sluggable?: scalar|null, // Default: "Gedmo\\Sluggable\\SluggableListener"
+ *         tree?: scalar|null, // Default: "Gedmo\\Tree\\TreeListener"
+ *         loggable?: scalar|null, // Default: "Gedmo\\Loggable\\LoggableListener"
+ *         sortable?: scalar|null, // Default: "Gedmo\\Sortable\\SortableListener"
+ *         softdeleteable?: scalar|null, // Default: "Gedmo\\SoftDeleteable\\SoftDeleteableListener"
+ *         uploadable?: scalar|null, // Default: "Gedmo\\Uploadable\\UploadableListener"
+ *         reference_integrity?: scalar|null, // Default: "Gedmo\\ReferenceIntegrity\\ReferenceIntegrityListener"
+ *     },
+ *     softdeleteable?: array{
+ *         handle_post_flush_event?: bool, // Default: false
+ *     },
+ *     uploadable?: array{
+ *         default_file_path?: scalar|null, // Default: null
+ *         mime_type_guesser_class?: scalar|null, // Default: "Stof\\DoctrineExtensionsBundle\\Uploadable\\MimeTypeGuesserAdapter"
+ *         default_file_info_class?: scalar|null, // Default: "Stof\\DoctrineExtensionsBundle\\Uploadable\\UploadedFileInfo"
+ *         validate_writable_directory?: bool, // Default: true
+ *     },
+ *     default_locale?: scalar|null, // Default: "en"
+ *     translation_fallback?: bool, // Default: false
+ *     persist_default_translation?: bool, // Default: false
+ *     skip_translation_on_load?: bool, // Default: false
+ *     metadata_cache_pool?: scalar|null, // Default: null
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1639,6 +1693,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *     twig_component?: TwigComponentConfig,
  *     live_component?: LiveComponentConfig,
  *     symfonycasts_sass?: SymfonycastsSassConfig,
+ *     stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1664,6 +1719,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         twig_component?: TwigComponentConfig,
  *         live_component?: LiveComponentConfig,
  *         symfonycasts_sass?: SymfonycastsSassConfig,
+ *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1687,6 +1743,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         twig_component?: TwigComponentConfig,
  *         live_component?: LiveComponentConfig,
  *         symfonycasts_sass?: SymfonycastsSassConfig,
+ *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
