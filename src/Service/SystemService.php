a7f2cca3f0f8d2f04b07c0a8f640f0c9f365fec9
Ярослав <yaroslaw74@gmail.com>
2025-12-27 16:27:07 +0200

    Update SystemService.php

--- src/Service/SystemService.php
+++ src/Service/SystemService.php
@@ -1,19 +1,35 @@
 <?php
-
-/**********************************************************************************
- * @Project    KYG Framework for Business
- * @Version    1.0.0
- *
- * @Copyright  (C) Kataev Yaroslav
- * @E-mail     yaroslaw74@gmail.com
- * @License    GNU General Public License version 3 or later, see LICENSE
- *********************************************************************************/
+/**
+ * @package    KYG Framework for Business
+ * @category   Service
+ * @version    1.0.0
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
 declare(strict_types=1);
-
 namespace App\Service;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 
 class SystemService
 {
+    public function __construct(private ContainerBagInterface $params)
+    {
+    }
+
+    public function getLocales(): array
+    {
+        return $this->params->get('app.locales');
+    }
+
+
+    public function getLocaleDir(string $locale): string
+    {
+        $locales = $this->getLocales();
+
+        return $locales[$locale]['dir'];
+    }
+
     public function getLocaleHTML(string $locale): string
     {
         str_contains($locale, '_') ? $html = str_replace('_', '-', $locale) : $html = $locale;
