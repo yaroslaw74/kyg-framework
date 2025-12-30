611deeb7eeb410f426219e4595bdbbe82c8f88b6
Ярослав <yaroslaw74@gmail.com>
2025-12-30 22:10:45 +0200

    Update object-manager.php

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -5,7 +5,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__.'/../vendor/autoload.php';
 
-(new Dotenv())->bootEnv(__DIR__.'/../.env');
+$dotenv = new Dotenv();
+$dotenv->bootEnv(__DIR__.'/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $kernel->boot();
