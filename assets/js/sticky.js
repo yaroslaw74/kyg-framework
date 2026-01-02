670cffdffd0f7e95ed88696c242a89b407005e08
Ярослав <yaroslaw74@gmail.com>
2026-01-02 22:14:21 +0200

    Update sticky.js

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -1,16 +1,15 @@
-
 "use strict";
 (() => {
-  window.addEventListener('scroll', stickyFn);
-  var navbar = document.getElementById("sidebar");
-  var sticky = navbar.offsetTop;
-  function stickyFn() {
-    if (window.scrollY >= 75) {
-      navbar.classList.add("sticky-pin")
-    } else {
-      navbar.classList.remove("sticky-pin");
+    window.addEventListener("scroll", stickyFn);
+    var navbar = document.getElementById("sidebar");
+    var sticky = navbar.offsetTop;
+    function stickyFn() {
+        if (window.scrollY >= 75) {
+            navbar.classList.add("sticky-pin");
+        } else {
+            navbar.classList.remove("sticky-pin");
+        }
     }
-  }
-  window.addEventListener('scroll', stickyFn);
-  window.addEventListener('DOMContentLoaded', stickyFn);
+    window.addEventListener("scroll", stickyFn);
+    window.addEventListener("DOMContentLoaded", stickyFn);
 })();
