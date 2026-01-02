1985be20abb392d1d661ca2119211468b5ff48e8
Ярослав <yaroslaw74@gmail.com>
2026-01-02 22:14:14 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,100 +1,91 @@
 (function () {
-    'use strict';
+    "use strict";
     if (localStorage.getItem("valexdarktheme")) {
-        document.querySelector("html").setAttribute("data-theme-mode", "dark")
-        document.querySelector("html").setAttribute("data-menu-styles", "dark")
-        document.querySelector("html").setAttribute("data-header-styles", "dark")
-    }
-    if (localStorage.valexrtl) {
-        let html = document.querySelector('html');
-        html.setAttribute("dir", "rtl");
-        document.querySelector("#style")?.setAttribute("href", "../assets/libs/bootstrap/css/bootstrap.rtl.min.css");
+        document.querySelector("html").setAttribute("data-theme-mode", "dark");
+        document.querySelector("html").setAttribute("data-menu-styles", "dark");
+        document.querySelector("html").setAttribute("data-header-styles", "dark");
     }
     if (localStorage.getItem("valexlayout") == "horizontal") {
-        document.querySelector("html").setAttribute("data-nav-layout", "horizontal")
+        document.querySelector("html").setAttribute("data-nav-layout", "horizontal");
     }
 
-    if(localStorage.loaderEnable == 'true'){
-        document.querySelector("html").setAttribute("loader","enable");
-    }else{
-        if(!document.querySelector("html").getAttribute("loader")){
-            document.querySelector("html").setAttribute("loader","disable");
+    if (localStorage.loaderEnable == "true") {
+        document.querySelector("html").setAttribute("loader", "enable");
+    } else {
+        if (!document.querySelector("html").getAttribute("loader")) {
+            document.querySelector("html").setAttribute("loader", "disable");
         }
     }
 
     function localStorageBackup() {
-
         // if there is a value stored, update color picker and background color
         // Used to retrive the data from local storage
         if (localStorage.primaryRGB) {
-            if (document.querySelector('.theme-container-primary')) {
-                document.querySelector('.theme-container-primary').value = localStorage.primaryRGB;
+            if (document.querySelector(".theme-container-primary")) {
+                document.querySelector(".theme-container-primary").value = localStorage.primaryRGB;
             }
-            document.querySelector('html').style.setProperty('--primary-rgb', localStorage.primaryRGB);
+            document.querySelector("html").style.setProperty("--primary-rgb", localStorage.primaryRGB);
         }
         if (localStorage.bodyBgRGB && localStorage.bodylightRGB) {
-            if (document.querySelector('.theme-container-background')) {
-                document.querySelector('.theme-container-background').value = localStorage.bodyBgRGB;
+            if (document.querySelector(".theme-container-background")) {
+                document.querySelector(".theme-container-background").value = localStorage.bodyBgRGB;
             }
-            document.querySelector('html').style.setProperty('--body-bg-rgb', localStorage.bodyBgRGB);
-            document.querySelector('html').style.setProperty('--body-bg-rgb2', localStorage.bodylightRGB);
-            document.querySelector('html').style.setProperty('--light-rgb', localStorage.bodylightRGB);
-            document.querySelector('html').style.setProperty('--form-control-bg', `rgb(${localStorage.bodylightRGB})`);
-            document.querySelector('html').style.setProperty('--input-border', "rgba(255,255,255,0.1)");
-            let html = document.querySelector('html');
-            html.setAttribute('data-theme-mode', 'dark');
-            html.setAttribute('data-menu-styles', 'dark');
-            html.setAttribute('data-header-styles', 'dark');
+            document.querySelector("html").style.setProperty("--body-bg-rgb", localStorage.bodyBgRGB);
+            document.querySelector("html").style.setProperty("--body-bg-rgb2", localStorage.bodylightRGB);
+            document.querySelector("html").style.setProperty("--light-rgb", localStorage.bodylightRGB);
+            document.querySelector("html").style.setProperty("--form-control-bg", `rgb(${localStorage.bodylightRGB})`);
+            document.querySelector("html").style.setProperty("--input-border", "rgba(255,255,255,0.1)");
+            let html = document.querySelector("html");
+            html.setAttribute("data-theme-mode", "dark");
+            html.setAttribute("data-menu-styles", "dark");
+            html.setAttribute("data-header-styles", "dark");
         }
         if (localStorage.valexdarktheme) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-theme-mode', 'dark');
+            let html = document.querySelector("html");
+            html.setAttribute("data-theme-mode", "dark");
         }
         if (localStorage.valexlayout) {
-            let html = document.querySelector('html');
-            let layoutValue = localStorage.getItem('valexlayout');
-            html.setAttribute('data-nav-layout', 'horizontal');
+            let html = document.querySelector("html");
+            let layoutValue = localStorage.getItem("valexlayout");
+            html.setAttribute("data-nav-layout", "horizontal");
             setTimeout(() => {
                 clearNavDropdown();
             }, 5000);
-            html.setAttribute('data-nav-style', 'menu-click');
+            html.setAttribute("data-nav-style", "menu-click");
             setTimeout(() => {
                 checkHoriMenu();
             }, 5000);
         }
         if (localStorage.valexverticalstyles) {
-            let html = document.querySelector('html');
-            let verticalStyles = localStorage.getItem('valexverticalstyles');
+            let html = document.querySelector("html");
+            let verticalStyles = localStorage.getItem("valexverticalstyles");
 
-            if (verticalStyles == 'default') {
-                html.setAttribute('data-vertical-style', 'default');
-                localStorage.removeItem("ynexnavstyles")
+            if (verticalStyles == "default") {
+                html.setAttribute("data-vertical-style", "default");
+                localStorage.removeItem("ynexnavstyles");
             }
 
-            if (verticalStyles == 'closed') {
-                html.setAttribute('data-vertical-style', 'closed');
-                localStorage.removeItem("valexnavstyles")
+            if (verticalStyles == "closed") {
+                html.setAttribute("data-vertical-style", "closed");
+                localStorage.removeItem("valexnavstyles");
             }
-            if (verticalStyles == 'icontext') {
-                html.setAttribute('data-vertical-style', 'icontext');
-                localStorage.removeItem("valexnavstyles")
+            if (verticalStyles == "icontext") {
+                html.setAttribute("data-vertical-style", "icontext");
+                localStorage.removeItem("valexnavstyles");
             }
-            if (verticalStyles == 'overlay') {
-                html.setAttribute('data-vertical-style', 'overlay');
-                localStorage.removeItem("valexnavstyles")
+            if (verticalStyles == "overlay") {
+                html.setAttribute("data-vertical-style", "overlay");
+                localStorage.removeItem("valexnavstyles");
             }
-            if (verticalStyles == 'detached') {
-                html.setAttribute('data-vertical-style', 'detached');
-                localStorage.removeItem("valexnavstyles")
+            if (verticalStyles == "detached") {
+                html.setAttribute("data-vertical-style", "detached");
+                localStorage.removeItem("valexnavstyles");
             }
-            if (verticalStyles == 'doublemenu') {
-                html.setAttribute('data-vertical-style', 'doublemenu');
-                localStorage.removeItem("valexnavstyles")
+            if (verticalStyles == "doublemenu") {
+                html.setAttribute("data-vertical-style", "doublemenu");
+                localStorage.removeItem("valexnavstyles");
                 setTimeout(() => {
-
-                    const menuSlideItem = document.querySelectorAll(
-                        ".main-menu > li > .side-menu__item"
-                    );
+                    const menuSlideItem = document.querySelectorAll(".main-menu > li > .side-menu__item");
 
                     // Create the tooltip element
                     const tooltip = document.createElement("div");
@@ -117,9 +108,8 @@
                         // Add an event listener to the menu slide item to show the tooltip
                         e.addEventListener("mouseenter", () => {
                             tooltip.style.setProperty("display", "block");
-                            tooltip.textContent =
-                                e.querySelector(".side-menu__label").textContent;
-                            if ( document.querySelector("html").getAttribute("data-vertical-style") == "doublemenu") {
+                            tooltip.textContent = e.querySelector(".side-menu__label").textContent;
+                            if (document.querySelector("html").getAttribute("data-vertical-style") == "doublemenu") {
                                 e.appendChild(tooltip);
                             }
                         });
@@ -127,106 +117,105 @@
                         // Add an event listener to hide the tooltip
                         e.addEventListener("mouseleave", () => {
                             tooltip.style.setProperty("display", "none");
-                            tooltip.textContent =e.querySelector(".side-menu__label").textContent;
-                            
+                            tooltip.textContent = e.querySelector(".side-menu__label").textContent;
                         });
                     });
                 }, 1000);
             }
         }
         if (localStorage.valexnavstyles) {
-            let html = document.querySelector('html');
-            let navStyles = localStorage.getItem('valexnavstyles');
-            if (navStyles == 'menu-click') {
-                html.setAttribute('data-nav-style', 'menu-click');
+            let html = document.querySelector("html");
+            let navStyles = localStorage.getItem("valexnavstyles");
+            if (navStyles == "menu-click") {
+                html.setAttribute("data-nav-style", "menu-click");
                 localStorage.removeItem("valexverticalstyles");
-                html.removeAttribute('data-vertical-style');
+                html.removeAttribute("data-vertical-style");
             }
-            if (navStyles == 'menu-hover') {
-                html.setAttribute('data-nav-style', 'menu-hover');
+            if (navStyles == "menu-hover") {
+                html.setAttribute("data-nav-style", "menu-hover");
                 localStorage.removeItem("valexverticalstyles");
-                html.removeAttribute('data-vertical-style');
+                html.removeAttribute("data-vertical-style");
             }
-            if (navStyles == 'icon-click') {
-                html.setAttribute('data-nav-style', 'icon-click');
+            if (navStyles == "icon-click") {
+                html.setAttribute("data-nav-style", "icon-click");
                 localStorage.removeItem("valexverticalstyles");
-                html.removeAttribute('data-vertical-style');
+                html.removeAttribute("data-vertical-style");
             }
-            if (navStyles == 'icon-hover') {
-                html.setAttribute('data-nav-style', 'icon-hover');
+            if (navStyles == "icon-hover") {
+                html.setAttribute("data-nav-style", "icon-hover");
                 localStorage.removeItem("valexverticalstyles");
-                html.removeAttribute('data-vertical-style');
+                html.removeAttribute("data-vertical-style");
             }
         }
         if (localStorage.valexclassic) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-page-style', 'classic'); 
+            let html = document.querySelector("html");
+            html.setAttribute("data-page-style", "classic");
         }
         if (localStorage.valexmodern) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-page-style', 'modern');
+            let html = document.querySelector("html");
+            html.setAttribute("data-page-style", "modern");
         }
         if (localStorage.valexboxed) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-width', 'boxed');
+            let html = document.querySelector("html");
+            html.setAttribute("data-width", "boxed");
         }
         if (localStorage.valexHeaderfixed) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-header-position', 'fixed');
+            let html = document.querySelector("html");
+            html.setAttribute("data-header-position", "fixed");
         }
         if (localStorage.valexHeaderscrollable) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-header-position', 'scrollable');
+            let html = document.querySelector("html");
+            html.setAttribute("data-header-position", "scrollable");
         }
         if (localStorage.valexMenufixed) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-menu-position', 'fixed');
+            let html = document.querySelector("html");
+            html.setAttribute("data-menu-position", "fixed");
         }
         if (localStorage.valexMenuscrollable) {
-            let html = document.querySelector('html');
-            html.setAttribute('data-menu-position', 'scrollable');
+            let html = document.querySelector("html");
+            html.setAttribute("data-menu-position", "scrollable");
         }
         if (localStorage.valexMenu) {
-            let html = document.querySelector('html');
-            let menuValue = localStorage.getItem('valexMenu');
+            let html = document.querySelector("html");
+            let menuValue = localStorage.getItem("valexMenu");
             switch (menuValue) {
-                case 'light':
-                    html.setAttribute('data-menu-styles', 'light');
+                case "light":
+                    html.setAttribute("data-menu-styles", "light");
                     break;
-                case 'dark':
-                    html.setAttribute('data-menu-styles', 'dark');
+                case "dark":
+                    html.setAttribute("data-menu-styles", "dark");
                     break;
-                case 'color':
-                    html.setAttribute('data-menu-styles', 'color');
+                case "color":
+                    html.setAttribute("data-menu-styles", "color");
                     break;
-                case 'gradient':
-                    html.setAttribute('data-menu-styles', 'gradient');
+                case "gradient":
+                    html.setAttribute("data-menu-styles", "gradient");
                     break;
-                case 'transparent':
-                    html.setAttribute('data-menu-styles', 'transparent');
+                case "transparent":
+                    html.setAttribute("data-menu-styles", "transparent");
                     break;
                 default:
                     break;
             }
         }
         if (localStorage.valexHeader) {
-            let html = document.querySelector('html');
-            let headerValue = localStorage.getItem('valexHeader');
+            let html = document.querySelector("html");
+            let headerValue = localStorage.getItem("valexHeader");
             switch (headerValue) {
-                case 'light':
-                    html.setAttribute('data-header-styles', 'light');
+                case "light":
+                    html.setAttribute("data-header-styles", "light");
                     break;
-                case 'dark':
-                    html.setAttribute('data-header-styles', 'dark');
+                case "dark":
+                    html.setAttribute("data-header-styles", "dark");
                     break;
-                case 'color':
-                    html.setAttribute('data-header-styles', 'color');
+                case "color":
+                    html.setAttribute("data-header-styles", "color");
                     break;
-                case 'gradient':
-                    html.setAttribute('data-header-styles', 'gradient');
+                case "gradient":
+                    html.setAttribute("data-header-styles", "gradient");
                     break;
-                case 'transparent':
-                    html.setAttribute('data-header-styles', 'transparent');
+                case "transparent":
+                    html.setAttribute("data-header-styles", "transparent");
                     break;
 
                 default:
@@ -234,11 +223,10 @@
             }
         }
         if (localStorage.bgimg) {
-            let html = document.querySelector('html');
-            let value = localStorage.getItem('bgimg');
-            html.setAttribute('data-bg-img', value);
+            let html = document.querySelector("html");
+            let value = localStorage.getItem("bgimg");
+            html.setAttribute("data-bg-img", value);
         }
     }
-    localStorageBackup()        
-
+    localStorageBackup();
 })();
