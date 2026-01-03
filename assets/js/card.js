b44d5793101dd45f9578a29f003a64981fa100cc
Ярослав <yaroslaw74@gmail.com>
2026-01-03 15:26:00 +0200

    Update card.js

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,12 +1,11 @@
 (() => {
     /* card with close button */
-    let DIV_CARD = ".card";
-    let cardRemoveBtn = document.querySelectorAll('[data-bs-toggle="card-remove"]');
+    const DIV_CARD = ".card";
+    const cardRemoveBtn = document.querySelectorAll('[data-bs-toggle="card-remove"]');
     cardRemoveBtn.forEach((ele) => {
         ele.addEventListener("click", function (e) {
             e.preventDefault();
-            let $this = this;
-            let card = $this.closest(DIV_CARD);
+            const card = this.closest(DIV_CARD);
             card.remove();
             return false;
         });
@@ -14,11 +13,10 @@
     /* card with close button */
 
     /* card with fullscreen */
-    let cardFullscreenBtn = document.querySelectorAll('[data-bs-toggle="card-fullscreen"]');
+    const cardFullscreenBtn = document.querySelectorAll('[data-bs-toggle="card-fullscreen"]');
     cardFullscreenBtn.forEach((ele) => {
         ele.addEventListener("click", function (e) {
-            let $this = this;
-            let card = $this.closest(DIV_CARD);
+            const card = this.closest(DIV_CARD);
             card.classList.toggle("card-fullscreen");
             card.classList.remove("card-collapsed");
             e.preventDefault();
