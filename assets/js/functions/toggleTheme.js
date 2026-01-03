89bc79047e50c208b0889e6244d2c5f53d335958
Ярослав <yaroslaw74@gmail.com>
2026-01-03 20:35:12 +0200

    Update toggleTheme.js

--- assets/js/functions/toggleTheme.js
+++ assets/js/functions/toggleTheme.js
@@ -1,56 +1,73 @@
+import { checkOptions } from "./checkOptions.js";
+
 export function toggleTheme() {
-    const html = document.querySelector("html");
-    if (html.getAttribute("data-theme-mode") === "dark") {
-        html.setAttribute("data-theme-mode", "light");
-        html.setAttribute("data-header-styles", "light");
-        html.setAttribute("data-menu-styles", "light");
+    const HTML = document.querySelector("HTML");
+
+    const LIGHT_BTN = document.querySelector("#switcher-light-theme");
+    const DARK_BTN = document.querySelector("#switcher-dark-theme");
+    const LIGHT_HEADER_BTN = document.querySelector("#switcher-header-light");
+    const DARK_HEADER_BTN = document.querySelector("#switcher-header-dark");
+    const LIGHT_MENU_BTN = document.querySelector("#switcher-menu-light");
+    const DARK_MENU_BTN = document.querySelector("#switcher-menu-dark");
+    const BG_DEFAULT_COLOR_1_BTN = document.querySelector("#switcher-background1");
+    const BG_DEFAULT_COLOR_2_BTN = document.querySelector("#switcher-background2");
+    const BG_DEFAULT_COLOR_3_BTN = document.querySelector("#switcher-background3");
+    const BG_DEFAULT_COLOR_4_BTN = document.querySelector("#switcher-background4");
+    const BG_DEFAULT_COLOR_5_BTN = document.querySelector("#switcher-background5");
+
+    if (HTML.getAttribute("data-theme-mode") === "dark") {
+        HTML.setAttribute("data-bs-theme", "light");
+        HTML.setAttribute("data-theme-mode", "light");
+        HTML.setAttribute("data-header-styles", "light");
+        HTML.setAttribute("data-menu-styles", "light");
         if (!localStorage.getItem("primaryRGB")) {
-            html.setAttribute("style", "");
+            HTML.setAttribute("style", "");
         }
-        html.removeAttribute("data-bg-theme");
-        document.querySelector("#switcher-light-theme").checked = true;
-        document.querySelector("#switcher-menu-light").checked = true;
-        html.style.removeProperty("--body-bg-rgb", localStorage.bodyBgRGB);
+        HTML.removeAttribute("data-bg-theme");
+        LIGHT_BTN.checked = true;
+        LIGHT_MENU_BTN.checked = true;
+        HTML.style.removeProperty("--body-bg-rgb", localStorage.getItem("bodyBgRGB"));
         checkOptions();
-        html.style.removeProperty("--light-rgb");
-        html.style.removeProperty("--form-control-bg");
-        html.style.removeProperty("--input-border");
-        document.querySelector("#switcher-header-light").checked = true;
-        document.querySelector("#switcher-menu-light").checked = true;
-        document.querySelector("#switcher-light-theme").checked = true;
-        document.querySelector("#switcher-background4").checked = false;
-        document.querySelector("#switcher-background3").checked = false;
-        document.querySelector("#switcher-background2").checked = false;
-        document.querySelector("#switcher-background1").checked = false;
-        document.querySelector("#switcher-background").checked = false;
+        HTML.style.removeProperty("--light-rgb");
+        HTML.style.removeProperty("--form-control-bg");
+        HTML.style.removeProperty("--input-border");
+        LIGHT_HEADER_BTN.checked = true;
+        LIGHT_MENU_BTN.checked = true;
+        LIGHT_BTN.checked = true;
+        BG_DEFAULT_COLOR_1_BTN.checked = false;
+        BG_DEFAULT_COLOR_2_BTN.checked = false;
+        BG_DEFAULT_COLOR_3_BTN.checked = false;
+        BG_DEFAULT_COLOR_4_BTN.checked = false;
+        BG_DEFAULT_COLOR_5_BTN.checked = false;
         localStorage.removeItem("valexdarktheme");
         localStorage.removeItem("valexMenu");
         localStorage.removeItem("valexHeader");
         localStorage.removeItem("bodylightRGB");
         localStorage.removeItem("bodyBgRGB");
         if (localStorage.getItem("valexlayout") !== "horizontal") {
-            html.setAttribute("data-menu-styles", "light");
+            HTML.setAttribute("data-menu-styles", "light");
         }
-        html.setAttribute("data-header-styles", "light");
+        HTML.setAttribute("data-header-styles", "light");
     } else {
-        html.setAttribute("data-theme-mode", "dark");
-        html.setAttribute("data-header-styles", "dark");
+        HTML.setAttribute("data-bs-theme", "dark");
+        HTML.setAttribute("data-theme-mode", "dark");
+        HTML.setAttribute("data-header-styles", "dark");
         if (!localStorage.getItem("primaryRGB")) {
-            html.setAttribute("style", "");
+            HTML.setAttribute("style", "");
         }
-        html.setAttribute("data-menu-styles", "dark");
-        document.querySelector("#switcher-dark-theme").checked = true;
-        document.querySelector("#switcher-menu-dark").checked = true;
-        document.querySelector("#switcher-header-dark").checked = true;
+        HTML.setAttribute("data-menu-styles", "dark");
+        DARK_BTN.checked = true;
+        DARK_MENU_BTN.checked = true;
+        DARK_HEADER_BTN.checked = true;
         checkOptions();
-        document.querySelector("#switcher-menu-dark").checked = true;
-        document.querySelector("#switcher-header-dark").checked = true;
-        document.querySelector("#switcher-dark-theme").checked = true;
-        document.querySelector("#switcher-background4").checked = false;
-        document.querySelector("#switcher-background3").checked = false;
-        document.querySelector("#switcher-background2").checked = false;
-        document.querySelector("#switcher-background1").checked = false;
-        document.querySelector("#switcher-background").checked = false;
+        DARK_MENU_BTN.checked = true;
+        DARK_HEADER_BTN.checked = true;
+        DARK_BTN.checked = true;
+        BG_DEFAULT_COLOR_1_BTN.checked = false;
+        BG_DEFAULT_COLOR_2_BTN.checked = false;
+        BG_DEFAULT_COLOR_3_BTN.checked = false;
+        BG_DEFAULT_COLOR_4_BTN.checked = false;
+        BG_DEFAULT_COLOR_5_BTN.checked = false;
         localStorage.setItem("valexdarktheme", "true");
         localStorage.setItem("valexMenu", "dark");
         localStorage.setItem("valexHeader", "dark");
