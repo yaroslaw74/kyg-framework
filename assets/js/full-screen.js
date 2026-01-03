9ed44d791eeac62a02a668fc210850e177422282
Ярослав <yaroslaw74@gmail.com>
2026-01-03 15:11:08 +0200

    Create full-screen.js

---
+++ assets/js/full-screen.js
@@ -0,0 +1,35 @@
+let elem = document.documentElement;
+function openFullscreen() {
+    let open = document.querySelector(".full-screen-open");
+    let close = document.querySelector(".full-screen-close");
+
+    if (!document.fullscreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) {
+            /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            /* IE11 */
+            elem.msRequestFullscreen();
+        }
+        close.classList.add("d-block");
+        close.classList.remove("d-none");
+        open.classList.add("d-none");
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            /* Safari */
+            document.webkitExitFullscreen();
+            console.log("working");
+        } else if (document.msExitFullscreen) {
+            /* IE11 */
+            document.msExitFullscreen();
+        }
+        close.classList.remove("d-block");
+        open.classList.remove("d-none");
+        close.classList.add("d-none");
+        open.classList.add("d-block");
+    }
+}
