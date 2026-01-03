625925358f5f2248019c85ab6ea2f9d59d5f757b
Ярослав <yaroslaw74@gmail.com>
2026-01-03 20:34:36 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,26 +4,27 @@ import Choices from "choices.js";
 import Waves from "node-waves";
 import SimpleBar from "simplebar";
 import { toggleTheme } from "./functions/toggleTheme.js";
+import { updateColors } from "./functions/updateColors.js";
 
 (() => {
-    const html = document.querySelector("html");
+    const HTML = document.querySelector("html");
 
     /* tooltip */
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const _tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+    const TOOLTIP_TRIGGER_LIST = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const _TOOLTIP_LIST_ = [...TOOLTIP_TRIGGER_LIST].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
 
     /* popover  */
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-    const _popoverList = [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
+    const POPOVER_TRIGGER_LIST = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const _POPOVER_LIST_ = [...POPOVER_TRIGGER_LIST].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
 
     //switcher color pickers
-    const pickrContainerPrimary = document.querySelector(".pickr-container-primary");
-    const themeContainerPrimary = document.querySelector(".theme-container-primary");
-    const pickrContainerBackground = document.querySelector(".pickr-container-background");
-    const themeContainerBackground = document.querySelector(".theme-container-background");
+    const PICKR_CONTAINER_PRIMARY = document.querySelector(".pickr-container-primary");
+    const THEME_CONTAINER_PRIMARY = document.querySelector(".theme-container-primary");
+    const PICKR_CONTAINER_BACKGROUND = document.querySelector(".pickr-container-background");
+    const THEME_CONTAINER_BACKGROUND = document.querySelector(".theme-container-background");
 
     /* for theme primary */
-    const nanoThemes = [
+    const NANO_THEMES = [
         [
             "nano",
             {
@@ -45,16 +46,16 @@ import { toggleTheme } from "./functions/toggleTheme.js";
             }
         ]
     ];
-    const nanoButtons = [];
+    const NANO_BUTTONS = [];
     let nanoPickr = null;
-    for (const [theme, config] of nanoThemes) {
-        const button = document.createElement("button");
-        button.innerHTML = theme;
-        nanoButtons.push(button);
+    for (const [THEME, CONFIG] of NANO_THEMES) {
+        const BUTTON = document.createElement("button");
+        BUTTON.innerHTML = THEME;
+        NANO_BUTTONS.push(BUTTON);
 
-        button.addEventListener("click", () => {
-            const el = document.createElement("p");
-            pickrContainerPrimary.appendChild(el);
+        BUTTON.addEventListener("click", () => {
+            const EL = document.createElement("p");
+            PICKR_CONTAINER_PRIMARY.appendChild(EL);
 
             /* Delete previous instance */
             if (nanoPickr) {
@@ -62,39 +63,39 @@ import { toggleTheme } from "./functions/toggleTheme.js";
             }
 
             /* Apply active class */
-            for (const btn of nanoButtons) {
-                btn.classList[btn === button ? "add" : "remove"]("active");
+            for (const BTN of NANO_BUTTONS) {
+                BTN.classList[BTN === BUTTON ? "add" : "remove"]("active");
             }
 
             /* Create fresh instance */
             nanoPickr = new Pickr(
                 Object.assign(
                     {
-                        el: el,
-                        theme: theme,
+                        el: EL,
+                        theme: THEME,
                         default: "#0162e8"
                     },
-                    config
+                    CONFIG
                 )
             );
 
             /* Set events */
             nanoPickr.on("changestop", (_source, instance) => {
-                const color = instance.getColor().toRGBA();
-                html.style.setProperty("--primary-rgb", `${Math.floor(color[0])}, ${Math.floor(color[1])}, ${Math.floor(color[2])}`);
+                const COLOR = instance.getColor().toRGBA();
+                HTML.style.setProperty("--primary-rgb", `${Math.floor(COLOR[0])}, ${Math.floor(COLOR[1])}, ${Math.floor(COLOR[2])}`);
                 /* theme color picker */
-                localStorage.setItem("primaryRGB", `${Math.floor(color[0])}, ${Math.floor(color[1])}, ${Math.floor(color[2])}`);
+                localStorage.setItem("primaryRGB", `${Math.floor(COLOR[0])}, ${Math.floor(COLOR[1])}, ${Math.floor(COLOR[2])}`);
                 updateColors();
             });
         });
 
-        themeContainerPrimary.appendChild(button);
+        THEME_CONTAINER_PRIMARY.appendChild(BUTTON);
     }
-    nanoButtons[0].click();
+    NANO_BUTTONS[0].click();
     /* for theme primary */
 
     /* for theme background */
-    const nanoThemes1 = [
+    const NANO_THEMES_1 = [
         [
             "nano",
             {
@@ -116,16 +117,16 @@ import { toggleTheme } from "./functions/toggleTheme.js";
             }
         ]
     ];
-    const nanoButtons1 = [];
+    const NANO_BUTTONS_1 = [];
     let nanoPickr1 = null;
-    for (const [theme, config] of nanoThemes1) {
-        const button = document.createElement("button");
-        button.innerHTML = theme;
-        nanoButtons1.push(button);
+    for (const [THEME, CONFIG] of NANO_THEMES_1) {
+        const BUTTON = document.createElement("button");
+        BUTTON.innerHTML = THEME;
+        NANO_BUTTONS_1.push(BUTTON);
 
-        button.addEventListener("click", () => {
-            const el = document.createElement("p");
-            pickrContainerBackground.appendChild(el);
+        BUTTON.addEventListener("click", () => {
+            const EL = document.createElement("p");
+            PICKR_CONTAINER_BACKGROUND.appendChild(EL);
 
             /* Delete previous instance */
             if (nanoPickr1) {
@@ -133,53 +134,55 @@ import { toggleTheme } from "./functions/toggleTheme.js";
             }
 
             /* Apply active class */
-            for (const btn of nanoButtons1) {
-                btn.classList[btn === button ? "add" : "remove"]("active");
+            for (const BTN of NANO_BUTTONS_1) {
+                BTN.classList[BTN === BUTTON ? "add" : "remove"]("active");
             }
 
             /* Create fresh instance */
             nanoPickr1 = new Pickr(
                 Object.assign(
                     {
-                        el: el,
-                        theme: theme,
+                        el: EL,
+                        theme: THEME,
                         default: "#0162e8"
                     },
-                    config
+                    CONFIG
                 )
             );
 
             /* Set events */
             nanoPickr1.on("changestop", (_source, instance) => {
-                const color = instance.getColor().toRGBA();
-                html.style.setProperty("--body-bg-rgb", `${color[0]}, ${color[1]}, ${color[2]}`);
-                html.style.setProperty("--light-rgb", `${color[0] + 14}, ${color[1] + 14}, ${color[2] + 14}`);
-                html.style.setProperty("--form-control-bg", `rgb(${color[0] + 14}, ${color[1] + 14}, ${color[2] + 14})`);
+                const COLOR = instance.getColor().toRGBA();
+                HTML.style.setProperty("--body-bg-rgb", `${COLOR[0]}, ${COLOR[1]}, ${COLOR[2]}`);
+                HTML.style.setProperty("--light-rgb", `${COLOR[0] + 14}, ${COLOR[1] + 14}, ${COLOR[2] + 14}`);
+                HTML.style.setProperty("--form-control-bg", `rgb(${COLOR[0] + 14}, ${COLOR[1] + 14}, ${COLOR[2] + 14})`);
                 localStorage.removeItem("bgtheme");
                 updateColors();
-                html.setAttribute("data-theme-mode", "dark");
-                html.setAttribute("data-menu-styles", "dark");
-                html.setAttribute("data-header-styles", "dark");
-                document.querySelector("#switcher-dark-theme").checked = true;
-                localStorage.setItem("bodyBgRGB", `${color[0]}, ${color[1]}, ${color[2]}`);
-                localStorage.setItem("bodylightRGB", `${color[0] + 14}, ${color[1] + 14}, ${color[2] + 14}`);
+                HTML.setAttribute("data-bs-theme", "dark");
+                HTML.setAttribute("data-theme-mode", "dark");
+                HTML.setAttribute("data-menu-styles", "dark");
+                HTML.setAttribute("data-header-styles", "dark");
+                const DARK_BTN = document.querySelector("#switcher-dark-theme");
+                DARK_BTN.checked = true;
+                localStorage.setItem("bodyBgRGB", `${COLOR[0]}, ${COLOR[1]}, ${COLOR[2]}`);
+                localStorage.setItem("bodylightRGB", `${COLOR[0] + 14}, ${COLOR[1] + 14}, ${COLOR[2] + 14}`);
             });
         });
-        themeContainerBackground.appendChild(button);
+        THEME_CONTAINER_BACKGROUND.appendChild(BUTTON);
     }
-    nanoButtons1[0].click();
+    NANO_BUTTONS_1[0].click();
     /* for theme background */
 
     /* header theme toggle */
-    const layoutSetting = document.querySelector(".layout-setting");
-    layoutSetting.addEventListener("click", toggleTheme);
+    const LAYOUT_SETTING = document.querySelector(".layout-setting");
+    LAYOUT_SETTING.addEventListener("click", toggleTheme);
     /* header theme toggle */
 
     /* Choices JS */
     document.addEventListener("DOMContentLoaded", () => {
-        const genericExamples = document.querySelectorAll("[data-trigger]");
-        for (let i = 0; i < genericExamples.length; ++i) {
-            const element = genericExamples[i];
+        const GENERIC_EXAMPLES = document.querySelectorAll("[data-trigger]");
+        for (let i = 0; i < GENERIC_EXAMPLES.length; ++i) {
+            const element = GENERIC_EXAMPLES[i];
             new Choices(element, {
                 allowHTML: true,
                 placeholderValue: "This is a placeholder set in the config",
@@ -195,10 +198,10 @@ import { toggleTheme } from "./functions/toggleTheme.js";
     /* node waves */
 
     /* header dropdowns scroll */
-    const myHeadernotification = document.getElementById("header-notification-scroll");
-    new SimpleBar(myHeadernotification, { autoHide: true });
+    const MY_HEADER_NOTIFICATION = document.getElementById("header-notification-scroll");
+    new SimpleBar(MY_HEADER_NOTIFICATION, { autoHide: true });
 
-    const myHeaderCart = document.getElementById("header-cart-items-scroll");
-    new SimpleBar(myHeaderCart, { autoHide: true });
+    const MY_HEADER_CART = document.getElementById("header-cart-items-scroll");
+    new SimpleBar(MY_HEADER_CART, { autoHide: true });
     /* header dropdowns scroll */
 })();
