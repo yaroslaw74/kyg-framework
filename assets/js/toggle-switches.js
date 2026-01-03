31f6d9680f3ac9581280d8675eb742dd9f114696
Ярослав <yaroslaw74@gmail.com>
2026-01-03 20:35:21 +0200

    Update toggle-switches.js

--- assets/js/toggle-switches.js
+++ assets/js/toggle-switches.js
@@ -1,5 +1,5 @@
-const customSwitch = document.querySelectorAll(".toggle");
-customSwitch.forEach((e) =>
+const CUSTOM_SWITCH = document.querySelectorAll(".toggle");
+CUSTOM_SWITCH.forEach((e) =>
     e.addEventListener("click", () => {
         e.classList.toggle("on");
     })
