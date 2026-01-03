20dade68a017501f08863d2335a42612b8138375
Ярослав <yaroslaw74@gmail.com>
2026-01-03 15:10:49 +0200

    Create close-button.js

---
+++ assets/js/close-button.js
@@ -0,0 +1,37 @@
+/* for cart dropdown */
+const headerbtn = document.querySelectorAll(".dropdown-item-close");
+headerbtn.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        button.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+        document.getElementById("cart-data").innerText = `${document.querySelectorAll(".dropdown-item-close").length} Items`;
+        document.getElementById("cart-icon-badge").innerText = `${document.querySelectorAll(".dropdown-item-close").length}`;
+        console.log(document.getElementById("header-cart-items-scroll").children.length);
+        if (document.querySelectorAll(".dropdown-item-close").length == 0) {
+            let elementHide = document.querySelector(".empty-header-item");
+            let elementShow = document.querySelector(".empty-item");
+            elementHide.classList.add("d-none");
+            elementShow.classList.remove("d-none");
+        }
+    });
+});
+/* for cart dropdown */
+
+/* for notifications dropdown */
+const headerbtn1 = document.querySelectorAll(".dropdown-item-close1");
+headerbtn1.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        button.parentNode.parentNode.parentNode.parentNode.remove();
+        document.getElementById("notifiation-data").innerText = `${document.querySelectorAll(".dropdown-item-close1").length} Unread`;
+        document.getElementById("notification-icon-badge").innerText = `${document.querySelectorAll(".dropdown-item-close1").length}`;
+        if (document.querySelectorAll(".dropdown-item-close1").length == 0) {
+            let elementHide1 = document.querySelector(".empty-header-item1");
+            let elementShow1 = document.querySelector(".empty-item1");
+            elementHide1.classList.add("d-none");
+            elementShow1.classList.remove("d-none");
+        }
+    });
+});
