c0c6d2fd5da015e330b30e87150f9e9b8aae06f6
Ярослав <yaroslaw74@gmail.com>
2026-01-04 22:03:17 +0200

    Update stickyFn.js

--- assets/js/functions/stickyFn.js
+++ assets/js/functions/stickyFn.js
@@ -1,8 +1,8 @@
 export function stickyFn() {
-    const NAV_BAR = document.getElementById("sidebar");
+    const NAVBAR = document.getElementById("sidebar");
     if (window.scrollY >= 75) {
-        NAV_BAR.classList.add("sticky-pin");
+        NAVBAR.classList.add("sticky-pin");
     } else {
-        NAV_BAR.classList.remove("sticky-pin");
+        NAVBAR.classList.remove("sticky-pin");
     }
 }
