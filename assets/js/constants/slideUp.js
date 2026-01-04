87166360894a1fd567382d017481a432d3b7e257
Ярослав <yaroslaw74@gmail.com>
2026-01-04 12:30:47 +0200

    Create slideUp.js

---
+++ assets/js/constants/slideUp.js
@@ -0,0 +1,26 @@
+export const slideUp = (target, duration = 300) => {
+    const { PARENT_ELEMENT } = target;
+    PARENT_ELEMENT.classList.remove("open");
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = `${duration}ms`;
+    target.style.boxSizing = "border-box";
+    target.style.height = `${target.offsetHeight}px`;
+    target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+        target.style.display = "none";
+        target.style.removeProperty("height");
+        target.style.removeProperty("padding-top");
+        target.style.removeProperty("padding-bottom");
+        target.style.removeProperty("margin-top");
+        target.style.removeProperty("margin-bottom");
+        target.style.removeProperty("overflow");
+        target.style.removeProperty("transition-duration");
+        target.style.removeProperty("transition-property");
+    }, duration);
+};
