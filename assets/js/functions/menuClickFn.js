c333c2189c4f51d8ba79158e8b9df30a7992a0f0
Ярослав <yaroslaw74@gmail.com>
2026-01-04 12:07:52 +0200

    Create menuClickFn.js

---
+++ assets/js/functions/menuClickFn.js
@@ -0,0 +1,14 @@
+import { toggleSidemenu } from "./toggleSidemenu.js";
+import { setNavActive } from "./setNavActive.js";
+
+export function menuClickFn() {
+    const HTML = document.querySelector("html");
+    HTML.setAttribute("data-nav-style", "menu-click");
+    HTML.removeAttribute("data-hor-style");
+    HTML.removeAttribute("data-vertical-style");
+
+    toggleSidemenu();
+    if (HTML.getAttribute("data-nav-layout") === "vertical") {
+        setNavActive();
+    }
+}
