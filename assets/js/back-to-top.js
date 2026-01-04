5593ef333e6f0da55b7fb3f227a83b1fde571505
Ярослав <yaroslaw74@gmail.com>
2026-01-04 22:02:16 +0200

    Update back-to-top.js

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,17 +1,16 @@
 (() => {
     const SCROLL_TO_TOP = document.querySelector(".scrollToTop");
-    const $_ROOT_ELEMENT = document.documentElement;
-    const _$_BODY = document.body;
+    const $ROOT_ELEMENT = document.documentElement;
+    const _$BODY = document.body;
     window.onscroll = () => {
         const _SCROLL_TOP = window.scrollY || window.pageYOffset;
-        const _CLIENT_HT = $_ROOT_ELEMENT.scrollHeight - $_ROOT_ELEMENT.clientHeight;
+        const _CLIENT_HT = $ROOT_ELEMENT.scrollHeight - $ROOT_ELEMENT.clientHeight;
         if (window.scrollY > 100) {
             SCROLL_TO_TOP.style.display = "flex";
         } else {
             SCROLL_TO_TOP.style.display = "none";
         }
     };
-
     SCROLL_TO_TOP.onclick = () => {
         window.scrollTo(0, 0);
     };
