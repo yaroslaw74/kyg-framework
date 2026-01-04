36b4032f22479ecaf0deede751ac58993c7bfd1f
Ярослав <yaroslaw74@gmail.com>
2026-01-04 12:30:44 +0200

    Create slideToggle.js

---
+++ assets/js/constants/slideToggle.js
@@ -0,0 +1,18 @@
+import { slideUp } from "./slideUp.js";
+
+const slideToggle = (target, duration = 300) => {
+    const HTML = document.querySelector("html");
+    if (
+        !(
+            (HTML.getAttribute("data-nav-style") === "menu-hover" && HTML.getAttribute("data-toggled") === "menu-hover-closed" && window.innerWidth >= 992) ||
+            (HTML.getAttribute("data-nav-style") === "icon-hover" && HTML.getAttribute("data-toggled") === "icon-hover-closed" && window.innerWidth >= 992)
+        ) &&
+        target &&
+        target.nodeType !== 3
+    ) {
+        if (window.getComputedStyle(target).display === "none") {
+            return slideDown(target, duration);
+        }
+        return slideUp(target, duration);
+    }
+};
