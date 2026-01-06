5e1c37a3c1ce06efcd0176bbdfa2957e3c5de0e7
Ярослав <yaroslaw74@gmail.com>
2026-01-06 22:20:25 +0200

    Update doublemenu.js

--- assets/js/functions/doublemenu.js
+++ assets/js/functions/doublemenu.js
@@ -2,8 +2,8 @@ import { doubleClickFn } from "./doubleClickFn.js";
 
 export function doublemenu() {
     if (window.innerWidth >= 992) {
-        const SIDEMENU_LINK = document.querySelectorAll(".main-menu > li > .side-menu__item");
-        SIDEMENU_LINK.forEach((ele) => {
+        const SIDE_MENU_LINK = document.querySelectorAll(".main-menu > li > .side-menu__item");
+        SIDE_MENU_LINK.forEach((ele) => {
             ele.addEventListener("click", doubleClickFn);
         });
     }
