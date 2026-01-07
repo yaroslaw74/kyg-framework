3d3f6f8518eb302783e5bd2d71c1f6fb063f27b8
Ярослав <yaroslaw74@gmail.com>
2026-01-07 18:44:41 +0200

    Update switcherArrowFn.js

--- assets/js/functions/switcherArrowFn.js
+++ assets/js/functions/switcherArrowFn.js
@@ -2,17 +2,17 @@ import { checkHoriMenu } from "./checkHoriMenu.js";
 import { slideClick } from "./slideClick.js";
 
 export function switcherArrowFn() {
-    const HTML = document.querySelector("html");
+    const SLIDE_LEFT = document.querySelector(".slide-left");
+    const SLIDE_RIGHT = document.querySelector(".slide-right");
+
+    const MENU_NAV = document.querySelector(".main-menu");
+    const MAIN_CONTAINER_1 = document.querySelector(".main-sidebar");
 
     // used to remove is-expanded class and remove class on clicking arrow buttons
     checkHoriMenu();
 
-    const MENU_NAV = document.querySelector(".main-menu");
-    const SLIDE_LEFT = document.querySelector(".slide-left");
-    const SLIDE_RIGHT = document.querySelector(".slide-right");
-
     SLIDE_LEFT.addEventListener("click", () => {
-        const MAIN_CONTAINER_1 = document.querySelector(".main-sidebar");
+        const HTML = document.querySelector("html");
         const MARGIN_LEFT_VALUE = Math.ceil(Number(window.getComputedStyle(MENU_NAV).marginLeft.split("px")[0]));
         const MARGIN_RIGHT_VALUE = Math.ceil(Number(window.getComputedStyle(MENU_NAV).marginRight.split("px")[0]));
         const MAIN_CONTAINER_1_WIDTH = MAIN_CONTAINER_1.offsetWidth;
@@ -21,7 +21,6 @@ export function switcherArrowFn() {
                 if (MARGIN_LEFT_VALUE < 0 && !(Math.abs(MARGIN_LEFT_VALUE) < MAIN_CONTAINER_1_WIDTH)) {
                     MENU_NAV.style.marginRight = 0;
                     MENU_NAV.style.marginLeft = `${Number(MENU_NAV.style.marginLeft.split("px")[0]) + Math.abs(MAIN_CONTAINER_1_WIDTH)}px`;
-                    SLIDE_RIGHT.classList.remove("d-none");
                 } else if (MARGIN_LEFT_VALUE >= 0) {
                     MENU_NAV.style.marginLeft = "0px";
                     SLIDE_LEFT.classList.add("d-none");
@@ -53,10 +52,10 @@ export function switcherArrowFn() {
         }
 
         const ELEMENT = document.querySelector(".main-menu > .slide.open");
+        const ELEMENT_1 = document.querySelector(".main-menu > .slide.open >ul");
         if (ELEMENT) {
             ELEMENT.classList.remove("open");
         }
-        const ELEMENT_1 = document.querySelector(".main-menu > .slide.open >ul");
         if (ELEMENT_1) {
             ELEMENT_1.style.display = "none";
         }
@@ -65,9 +64,7 @@ export function switcherArrowFn() {
         return;
         //
     });
-
     SLIDE_RIGHT.addEventListener("click", () => {
-        const MAIN_CONTAINER_1 = document.querySelector(".main-sidebar");
         const MARGIN_LEFT_VALUE = Math.ceil(Number(window.getComputedStyle(MENU_NAV).marginLeft.split("px")[0]));
         const MARGIN_RIGHT_VALUE = Math.ceil(Number(window.getComputedStyle(MENU_NAV).marginRight.split("px")[0]));
         const CHECK = MENU_NAV.scrollWidth - MAIN_CONTAINER_1.offsetWidth;
@@ -98,10 +95,10 @@ export function switcherArrowFn() {
         }
 
         const ELEMENT = document.querySelector(".main-menu > .slide.open");
+        const ELEMENT_1 = document.querySelector(".main-menu > .slide.open >ul");
         if (ELEMENT) {
             ELEMENT.classList.remove("open");
         }
-        const ELEMENT_1 = document.querySelector(".main-menu > .slide.open >ul");
         if (ELEMENT_1) {
             ELEMENT_1.style.display = "none";
         }
