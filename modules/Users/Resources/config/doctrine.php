cc7c63ad274065f5560eafb3e317d8f5218028d5
Ярослав <yaroslaw74@gmail.com>
2026-01-07 22:12:47 +0200

    Create doctrine.php

---
+++ modules/Users/Resources/config/doctrine.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Config
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+use Symfony\Config\DoctrineConfig;
+
+return static function (DoctrineConfig $doctrine): void {
+    $emDefault = $doctrine->orm()->entityManager('default');
+    $emDefault->mapping('SomeEntityNamespace')
+        ->type('attribute')
+        ->dir('%kernel.project_dir%/modules/Users/Entity')
+        ->isBundle(false)
+        ->prefix('App\Modules\Users\Entity')
+        ->alias('Users')
+    ;
+};
