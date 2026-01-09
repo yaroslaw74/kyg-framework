c98965ef96d18c4d8d862f623d805fce7bad92e7
Ярослав <yaroslaw74@gmail.com>
2026-01-09 21:09:00 +0200

    Update authentication-main.js

--- assets/js/authentication-main.js
+++ assets/js/authentication-main.js
@@ -1,7 +1,6 @@
 import { localStorageBackup3 } from "./functions/localStorageBackup3.js";
 
-(function () {
-    "use strict";
+(() => {
     const HTML = document.querySelector("html");
     if (localStorage.getItem("valexdarktheme") === "true") {
         HTML.setAttribute("data-bs-theme", "dark");
