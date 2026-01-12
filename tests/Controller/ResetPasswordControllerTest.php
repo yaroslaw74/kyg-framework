84fc4f12b0e17c46b4d1a1aaae5923efc12e6599
Ярослав <yaroslaw74@gmail.com>
2026-01-12 21:56:40 +0200

    Create ResetPasswordControllerTest.php

---
+++ tests/Controller/ResetPasswordControllerTest.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Controller Test
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+
+namespace App\Tests\Controller;
+
+use App\Modules\Users\Entity\User;
+use App\Modules\Users\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class ResetPasswordControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $em;
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        // Ensure we have a clean database
+        $container = static::getContainer();
+
+        /** @var EntityManagerInterface $em */
+        $em = $container->get('doctrine')->getManager();
+        $this->em = $em;
+
+        $this->userRepository = $container->get(UserRepository::class);
+
+        foreach ($this->userRepository->findAll() as $user) {
+            $this->em->remove($user);
+        }
+
+        $this->em->flush();
+    }
+
+    public function testResetPasswordController(): void
+    {
+        // Create a test user
+        $user = (new User())
+            ->setEmail('me@example.com')
+            ->setPassword('a-test-password-that-will-be-changed-later')
+        ;
+        $this->em->persist($user);
+        $this->em->flush();
+
+        // Test Request reset password page
+        $this->client->request('GET', '/kyg/reset-password');
+
+        self::assertResponseIsSuccessful();
+        self::assertPageTitleContains('Reset your password');
+
+        // Submit the reset password form and test email message is queued / sent
+        $this->client->submitForm('Send password reset email', [
+            'reset_password_request_form[email]' => 'me@example.com',
+        ]);
+
+        // Ensure the reset password email was sent
+        // Use either assertQueuedEmailCount() || assertEmailCount() depending on your mailer setup
+        // self::assertQueuedEmailCount(1);
+        self::assertEmailCount(1);
+
+        self::assertCount(1, $messages = $this->getMailerMessages());
+
+        self::assertEmailAddressContains($messages[0], 'from', 'mailer@your-domain.com');
+        self::assertEmailAddressContains($messages[0], 'to', 'me@example.com');
+        self::assertEmailTextBodyContains($messages[0], 'This link will expire in 1 hour.');
+
+        self::assertResponseRedirects('/kyg/reset-password/check-email');
+
+        // Test check email landing page shows correct "expires at" time
+        $crawler = $this->client->followRedirect();
+
+        self::assertPageTitleContains('Password Reset Email Sent');
+        self::assertStringContainsString('This link will expire in 1 hour', $crawler->html());
+
+        // Test the link sent in the email is valid
+        $email = $messages[0]->toString();
+        preg_match('#(/kyg/reset-password/reset/[a-zA-Z0-9]+)#', $email, $resetLink);
+
+        $this->client->request('GET', $resetLink[1]);
+
+        self::assertResponseRedirects('/kyg/reset-password/reset');
+
+        $this->client->followRedirect();
+
+        // Test we can set a new password
+        $this->client->submitForm('Reset password', [
+            'change_password_form[plainPassword][first]' => 'newStrongPassword',
+            'change_password_form[plainPassword][second]' => 'newStrongPassword',
+        ]);
+
+        self::assertResponseRedirects('/kyg');
+
+        $user = $this->userRepository->findOneBy(['email' => 'me@example.com']);
+
+        self::assertInstanceOf(User::class, $user);
+
+        /** @var UserPasswordHasherInterface $passwordHasher */
+        $passwordHasher = static::getContainer()->get(UserPasswordHasherInterface::class);
+        self::assertTrue($passwordHasher->isPasswordValid($user, 'newStrongPassword'));
+    }
+}
