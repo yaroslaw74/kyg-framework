6bc93e2a8fd49dbe2ed2fb230ffbd7638edf0609
Ярослав <yaroslaw74@gmail.com>
2026-01-13 21:33:33 +0200

    Update RegistrationFormType.php

--- modules/Users/Form/RegistrationFormType.php
+++ modules/Users/Form/RegistrationFormType.php
@@ -14,39 +14,64 @@ namespace App\Modules\Users\Form;
 use App\Modules\Users\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationFormType extends AbstractType
 {
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                    'label' => 'Username',
+                    'row_attr' => ['class' => 'form-group mb-3'],
+                    'attr' => ['placeholder' => $this->translator->trans('Enter your username', [], 'users')],
+                ])
+            ->add('email', EmailType::class, [
+                    'label' => 'Email',
+                    'row_attr' => ['class' => 'form-group mb-3'],
+                    'attr' => ['placeholder' => $this->translator->trans('Enter your Email', [], 'users')],
+                ])
             ->add('agreeTerms', CheckboxType::class, [
+                    'label' => 'I agree to the terms and conditions',
+                    'row_attr' => ['class' => 'form-group mb-3'],
                     'mapped' => false,
                     'constraints' => [
                         new IsTrue([
-                            'message' => 'You should agree to our terms.',
+                            'message' => $this->translator->trans('You should agree to our terms.', [], 'users'),
                         ]),
                     ],
                 ])
             ->add('plainPassword', PasswordType::class, [
+                    'label' => 'Password',
+                    'row_attr' => ['class' => 'form-group mb-3'],
+                    'toggle' => true,
+                    'hidden_label' => 'Hide password',
+                    'visible_label' => 'Show password',
                     // instead of being set onto the object directly,
                     // this is read and encoded in the controller
                     'mapped' => false,
-                    'attr' => ['autocomplete' => 'new-password'],
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'placeholder' => $this->translator->trans('Enter your password', [], 'users')
+                    ],
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please enter a password',
+                            'message' => $this->translator->trans('Please enter a password', [], 'users'),
                         ]),
                         new Length([
                             'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            'minMessage' => $this->translator->trans('Your password should be at least {{ limit }} characters', [], 'users'),
                             // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
