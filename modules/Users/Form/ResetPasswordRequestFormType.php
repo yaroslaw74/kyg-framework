35d050df5d0f6b5b03a606cf76c4c5230b217ef6
Ярослав <yaroslaw74@gmail.com>
2026-01-13 22:03:52 +0200

    Update ResetPasswordRequestFormType.php

--- modules/Users/Form/ResetPasswordRequestFormType.php
+++ modules/Users/Form/ResetPasswordRequestFormType.php
@@ -16,9 +16,14 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ResetPasswordRequestFormType extends AbstractType
 {
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
