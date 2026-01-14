6926365e512652cc43b58e5c585ad506a1d56a6c
Ярослав <yaroslaw74@gmail.com>
2026-01-14 10:53:47 +0200

    Update ChangePasswordFormType.php

--- modules/Users/Form/ChangePasswordFormType.php
+++ modules/Users/Form/ChangePasswordFormType.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * KYG Framework for Business.
  *
@@ -9,6 +10,7 @@
  * @copyright  Copyright (c) Kataev Yaroslav
  * @license    GNU General Public License version 3 or later, see LICENSE
  */
+
 namespace App\Modules\Users\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -32,48 +34,48 @@ class ChangePasswordFormType extends AbstractType
     {
         $builder
             ->add('plainPassword', RepeatedType::class, [
-                    'row_attr' => [
-                        'class' => 'form-group mb-3'
+                'row_attr' => [
+                    'class' => 'form-group mb-3',
+                ],
+                'toggle' => true,
+                'hidden_label' => 'Hide password',
+                'visible_label' => 'Show password',
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
                     ],
-                    'toggle' => true,
-                    'hidden_label' => 'Hide password',
-                    'visible_label' => 'Show password',
-                    'type' => PasswordType::class,
-                    'options' => [
-                        'attr' => [
-                            'autocomplete' => 'new-password'
-                        ]
+                ],
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => $this->translator->trans('Please enter a password', [], 'users'),
+                        ]),
+                        new Length([
+                            'min' => 12,
+                            'minMessage' => $this->translator->trans('Your password should be at least {{ limit }} characters', [], 'users'),
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new PasswordStrength(),
+                        new NotCompromisedPassword(),
                     ],
-                    'first_options' => [
-                        'constraints' => [
-                            new NotBlank([
-                                'message' => $this->translator->trans('Please enter a password', [], 'users')
-                            ]),
-                            new Length([
-                                'min' => 12,
-                                'minMessage' => $this->translator->trans('Your password should be at least {{ limit }} characters', [], 'users'),
-                                // max length allowed by Symfony for security reasons
-                                'max' => 4096
-                            ]),
-                            new PasswordStrength(),
-                            new NotCompromisedPassword()
-                        ],
-                        'label' => 'New password',
-                        'attr' => [
-                            'placeholder' => $this->translator->trans('Enter your password', [], 'users')
-                        ]
+                    'label' => 'New password',
+                    'attr' => [
+                        'placeholder' => $this->translator->trans('Enter your password', [], 'users'),
                     ],
-                    'second_options' => [
-                        'label' => 'Repeat Password',
-                        'attr' => [
-                            'placeholder' => $this->translator->trans('Enter your Confirm Password', [], 'users')
-                        ]
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => [
+                        'placeholder' => $this->translator->trans('Enter your Confirm Password', [], 'users'),
                     ],
-                    'invalid_message' => $this->translator->trans('The password fields must match.', [], 'users'),
-                    // Instead of being set onto the object directly,
-                    // this is read and encoded in the controller
-                    'mapped' => false
-                ])
+                ],
+                'invalid_message' => $this->translator->trans('The password fields must match.', [], 'users'),
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
         ;
     }
 
