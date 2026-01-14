a7f3730facb8595ebfce9e6fd24e3a506439ffa5
Ярослав <yaroslaw74@gmail.com>
2026-01-14 10:14:50 +0200

    Update ChangePasswordFormType.php

--- modules/Users/Form/ChangePasswordFormType.php
+++ modules/Users/Form/ChangePasswordFormType.php
@@ -20,42 +20,59 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
 use Symfony\Component\Validator\Constraints\PasswordStrength;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ChangePasswordFormType extends AbstractType
 {
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('plainPassword', RepeatedType::class, [
+                    'row_attr' => [
+                        'class' => 'form-group mb-3'
+                    ],
+                    'toggle' => true,
+                    'hidden_label' => 'Hide password',
+                    'visible_label' => 'Show password',
                     'type' => PasswordType::class,
                     'options' => [
                         'attr' => [
-                            'autocomplete' => 'new-password',
-                        ],
+                            'autocomplete' => 'new-password'
+                        ]
                     ],
                     'first_options' => [
                         'constraints' => [
                             new NotBlank([
-                                'message' => 'Please enter a password',
+                                'message' => $this->translator->trans('Please enter a password', [], 'users')
                             ]),
                             new Length([
                                 'min' => 12,
-                                'minMessage' => 'Your password should be at least {{ limit }} characters',
+                                'minMessage' => $this->translator->trans('Your password should be at least {{ limit }} characters', [], 'users'),
                                 // max length allowed by Symfony for security reasons
-                                'max' => 4096,
+                                'max' => 4096
                             ]),
                             new PasswordStrength(),
-                            new NotCompromisedPassword(),
+                            new NotCompromisedPassword()
                         ],
                         'label' => 'New password',
+                        'attr' => [
+                            'placeholder' => $this->translator->trans('Enter your password', [], 'users')
+                        ]
                     ],
                     'second_options' => [
                         'label' => 'Repeat Password',
+                        'attr' => [
+                            'placeholder' => $this->translator->trans('Enter your Confirm Password', [], 'users')
+                        ]
                     ],
-                    'invalid_message' => 'The password fields must match.',
+                    'invalid_message' => $this->translator->trans('The password fields must match.', [], 'users'),
                     // Instead of being set onto the object directly,
                     // this is read and encoded in the controller
-                    'mapped' => false,
+                    'mapped' => false
                 ])
         ;
     }
