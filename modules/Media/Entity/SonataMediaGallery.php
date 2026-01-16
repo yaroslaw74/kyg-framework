e60c5244c00c60653350f0418fb8bc0faf4d1485
Ярослав <yaroslaw74@gmail.com>
2026-01-16 17:21:25 +0200

    Create SonataMediaGallery.php

---
+++ modules/Media/Entity/SonataMediaGallery.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Media\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Media\Repository\SonataMediaGalleryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\MediaBundle\Entity\BaseGallery;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: SonataMediaGalleryRepository::class)] // @phpstan-ignore missingType.generics
+#[ORM\Table(name: 'media__gallery')]
+#[ApiResource]
+class SonataMediaGallery extends BaseGallery
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    protected ?Uuid $id = null;
+
+    public function getId(): ?string
+    {
+        if (null === $this->id) {
+            return null;
+        }
+
+        return $this->id->toString();
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+}
