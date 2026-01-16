c10a6f096a07a879a52d6ee6f6e3fbf43f19f02c
Ярослав <yaroslaw74@gmail.com>
2026-01-16 17:21:35 +0200

    Create SonataMediaGalleryItemRepository.php

---
+++ modules/Media/Repository/SonataMediaGalleryItemRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Media\Repository;
+
+use App\Modules\Media\Entity\SonataMediaGalleryItem;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<SonataMediaGalleryItem>
+ */
+class SonataMediaGalleryItemRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SonataMediaGalleryItem::class);
+    }
+
+    /**
+     * @return SonataMediaGalleryItem[] Returns an array of SonataMediaGalleryItem objects
+     */
+    public function findByExampleField($value): array // @phpstan-ignore missingType.parameter
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findOneBySomeField($value): ?SonataMediaGalleryItem // @phpstan-ignore missingType.parameter
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
