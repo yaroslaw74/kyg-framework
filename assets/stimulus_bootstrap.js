de03e70b0c3201dee048071c0a6c1bef93557d9c
Ярослав <yaroslaw74@gmail.com>
2026-01-21 19:41:47 +0200

    Create stimulus_bootstrap.js

---
+++ assets/stimulus_bootstrap.js
@@ -0,0 +1,5 @@
+import { startStimulusApp } from "@symfony/stimulus-bundle";
+
+const _app = startStimulusApp();
+// register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
