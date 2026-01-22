de44c4ff24440858ada6125b41ea5211378f96e0
Ярослав <yaroslaw74@gmail.com>
2026-01-22 10:48:31 +0200

    Create AppController.php

---
+++ src/Controller/AppController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Controller
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AppController extends AbstractController
+{
+    #[Route('/app', name: 'app')]
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/app/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+}
