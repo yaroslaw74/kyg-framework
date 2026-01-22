ecbc531067b50f4079c63146d3c1604958b4aaf1
Ярослав <yaroslaw74@gmail.com>
2026-01-22 12:04:37 +0200

    Create authentication-main.js

---
+++ assets/js/authentication-main.js
@@ -0,0 +1,15 @@
+import { localStorageBackup3 } from "./functions/localStorageBackup3.js";
+
+(() => {
+    const HTML = document.querySelector("html");
+    if (localStorage.getItem("valexdarktheme") === "true") {
+        HTML.setAttribute("data-bs-theme", "dark");
+        HTML.setAttribute("data-theme-mode", "dark");
+        HTML.setAttribute("data-menu-styles", "dark");
+        HTML.setAttribute("data-header-styles", "dark");
+    }
+    if (localStorage.getItem("valexlayout") === "horizontal") {
+        HTML.setAttribute("data-nav-layout", "horizontal");
+    }
+    localStorageBackup3();
+})();
