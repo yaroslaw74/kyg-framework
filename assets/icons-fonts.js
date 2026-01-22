1629f7c67d9c70858ca2d1b8b407c5e26c7846b7
Ярослав <yaroslaw74@gmail.com>
2026-01-22 11:10:02 +0200

    Create icons-fonts.js

---
+++ assets/icons-fonts.js
@@ -0,0 +1,69 @@
+/*
+ * Bootstrap Icons
+ */
+import "./vendor/bootstrap-icons/font/bootstrap-icons.min.css";
+
+/*
+ * Boxicons
+ */
+import "./vendor/boxicons/css/boxicons.min.css";
+
+/*
+ * Feather Icons
+ */
+import "./icon-fonts/feather/feather.css";
+
+/*
+ * Font Awesome
+ */
+import "./vendor/@fortawesome/fontawesome-free/css/all.min.css";
+
+/*
+ * Ionicons
+ */
+import "./vendor/ionicons/dist/css/ionicons.min.css";
+
+/*
+ * Icons8 Line Awesome
+ */
+import "./vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css";
+
+/*
+ * Material Design Icons
+ */
+import "./vendor/@mdi/font/css/materialdesignicons.min.css";
+
+/*
+ * Stroke 7 Icon Font Set
+ */
+import "./vendor/pixeden-stroke-7-icon/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css";
+
+/*
+ * Remix Icon
+ */
+import "./vendor/remixicon/fonts/remixicon.css";
+
+/*
+ * Simple Line Icons
+ */
+import "./vendor/simple-line-icons/css/simple-line-icons.css";
+
+/*
+ * Tabler Icons Webfont
+ */
+import "./vendor/@tabler/icons-webfont/dist/tabler-icons.min.css";
+
+/*
+ * Themify Icons
+ */
+import "./vendor/@icon/themify-icons/themify-icons.css";
+
+/*
+ * Typicons
+ */
+import "./vendor/typicons.font/src/font/typicons.css";
+
+/*
+ * Weather Icons
+ */
+import "./vendor/weather-icons/css/weather-icons.min.css";
