ed46a71527ed4cd970842cee1a58ac378a921bd5
Ярослав <yaroslaw74@gmail.com>
2026-01-22 12:07:59 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+import { localStorageBackup } from "./functions/localStorageBackup.js";
+
+(() => {
+    const HTML = document.querySelector("html");
+
+    if (localStorage.getItem("valexdarktheme")) {
+        HTML.setAttribute("data-bs-theme", "dark");
+        HTML.setAttribute("data-theme-mode", "dark");
+        HTML.setAttribute("data-menu-styles", "dark");
+        HTML.setAttribute("data-header-styles", "dark");
+    }
+    if (localStorage.getItem("valexlayout") === "horizontal") {
+        HTML.setAttribute("data-nav-layout", "horizontal");
+    }
+
+    if (localStorage.getItem("loaderEnable") === "true") {
+        HTML.setAttribute("loader", "enable");
+    } else {
+        if (!HTML.getAttribute("loader")) {
+            HTML.setAttribute("loader", "enable");
+            localStorage.setItem("loaderEnable", "true");
+        }
+    }
+
+    localStorageBackup();
+})();
