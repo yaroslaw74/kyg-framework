4f44f51c972d395b322749ad960d2139276e0905
Ярослав <yaroslaw74@gmail.com>
2026-01-22 12:08:19 +0200

    Create plugins.js

---
+++ assets/plugins.js
@@ -0,0 +1,24 @@
+/*
+ * Node Waves Css
+ */
+import "./vendor/node-waves/dist/waves.min.css";
+
+/*
+ * Simplebar Css
+ */
+import "./vendor/simplebar/dist/simplebar.min.css";
+
+/*
+ * Flatpickr css
+ */
+import "./vendor/flatpickr/dist/flatpickr.min.css";
+
+/*
+ * Color Picker Css
+ */
+import "./vendor/@simonwep/pickr/dist/themes/nano.min.css";
+
+/*
+ * Choices Css
+ */
+import "./vendor/choices.js/public/assets/styles/choices.min.css";
