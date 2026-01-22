38ba1ad5aba1c82009ef0e65d7863749c9e567ff
Ярослав <yaroslaw74@gmail.com>
2026-01-22 11:10:07 +0200

    Update importmap.php

--- importmap.php
+++ importmap.php
@@ -19,6 +19,9 @@ return [
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
+    '@symfony/ux-live-component' => [
+        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
@@ -28,7 +31,48 @@ return [
     'es-module-shims' => [
         'version' => '2.8.0',
     ],
-    '@symfony/ux-live-component' => [
-        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    '@fortawesome/fontawesome-free/css/all.min.css' => [
+        'version' => '7.1.0',
+        'type' => 'css',
+    ],
+    'ionicons/dist/css/ionicons.min.css' => [
+        'version' => '4.6.4-1',
+        'type' => 'css',
+    ],
+    'line-awesome/dist/line-awesome/css/line-awesome.min.css' => [
+        'version' => '1.3.0',
+        'type' => 'css',
+    ],
+    '@mdi/font/css/materialdesignicons.min.css' => [
+        'version' => '7.4.47',
+        'type' => 'css',
+    ],
+    'pixeden-stroke-7-icon/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css' => [
+        'version' => '1.2.3',
+        'type' => 'css',
+    ],
+    'remixicon/fonts/remixicon.css' => [
+        'version' => '4.8.0',
+        'type' => 'css',
+    ],
+    'simple-line-icons/css/simple-line-icons.css' => [
+        'version' => '2.5.5',
+        'type' => 'css',
+    ],
+    '@tabler/icons-webfont/dist/tabler-icons.min.css' => [
+        'version' => '3.36.0',
+        'type' => 'css',
+    ],
+    '@icon/themify-icons/themify-icons.css' => [
+        'version' => '1.0.1-alpha.3',
+        'type' => 'css',
+    ],
+    'typicons.font/src/font/typicons.css' => [
+        'version' => '2.1.2',
+        'type' => 'css',
+    ],
+    'weather-icons/css/weather-icons.min.css' => [
+        'version' => '1.3.2',
+        'type' => 'css',
     ],
 ];
