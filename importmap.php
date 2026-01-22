72460f6235b5691793e922e2a6a350e7c4204834
Ярослав <yaroslaw74@gmail.com>
2026-01-22 12:08:53 +0200

    Update importmap.php

--- importmap.php
+++ importmap.php
@@ -16,6 +16,14 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'ltr' => [
+        'path' => './assets/ltr.js',
+        'entrypoint' => true,
+    ],
+    'rtl' => [
+        'path' => './assets/rtl.js',
+        'entrypoint' => true,
+    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
@@ -30,6 +38,14 @@ return [
     ],
     'es-module-shims' => [
         'version' => '2.8.0',
+    ],
+    'bootstrap-icons/font/bootstrap-icons.min.css' => [
+        'version' => '1.13.1',
+        'type' => 'css',
+    ],
+    'boxicons/css/boxicons.min.css' => [
+        'version' => '2.1.4',
+        'type' => 'css',
     ],
     '@fortawesome/fontawesome-free/css/all.min.css' => [
         'version' => '7.1.0',
@@ -60,7 +76,7 @@ return [
         'type' => 'css',
     ],
     '@tabler/icons-webfont/dist/tabler-icons.min.css' => [
-        'version' => '3.36.0',
+        'version' => '3.36.1',
         'type' => 'css',
     ],
     '@icon/themify-icons/themify-icons.css' => [
@@ -73,6 +89,119 @@ return [
     ],
     'weather-icons/css/weather-icons.min.css' => [
         'version' => '1.3.2',
+        'type' => 'css',
+    ],
+    '@symfony/ux-swup' => [
+        'version' => '2.32.0',
+    ],
+    '@swup/debug-plugin' => [
+        'version' => '4.1.0',
+    ],
+    '@swup/fade-theme' => [
+        'version' => '2.0.2',
+    ],
+    '@swup/forms-plugin' => [
+        'version' => '3.6.0',
+    ],
+    '@swup/slide-theme' => [
+        'version' => '2.0.2',
+    ],
+    'swup' => [
+        'version' => '4.8.2',
+    ],
+    '@swup/plugin' => [
+        'version' => '4.0.0',
+    ],
+    'delegate-it' => [
+        'version' => '6.3.0',
+    ],
+    '@swup/theme' => [
+        'version' => '2.1.0',
+    ],
+    'path-to-regexp' => [
+        'version' => '8.3.0',
+    ],
+    '@symfony/ux-toggle-password' => [
+        'version' => '2.32.0',
+    ],
+    '@symfony/ux-toggle-password/dist/style.min.css' => [
+        'version' => '2.32.0',
+        'type' => 'css',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
+    'bootstrap/dist/css/bootstrap.rtl.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
+    'node-waves' => [
+        'version' => '0.7.6',
+    ],
+    'node-waves/dist/waves.min.css' => [
+        'version' => '0.7.6',
+        'type' => 'css',
+    ],
+    'simplebar' => [
+        'version' => '6.3.3',
+    ],
+    'simplebar-core' => [
+        'version' => '1.3.2',
+    ],
+    'simplebar/dist/simplebar.min.css' => [
+        'version' => '6.3.3',
+        'type' => 'css',
+    ],
+    'lodash-es/debounce.js' => [
+        'version' => '4.17.21',
+    ],
+    'lodash-es/throttle.js' => [
+        'version' => '4.17.21',
+    ],
+    'resize-observer-polyfill' => [
+        'version' => '1.5.1',
+    ],
+    '@simonwep/pickr/dist/pickr.es5.min.js' => [
+        'version' => '1.9.1',
+    ],
+    '@simonwep/pickr/dist/themes/nano.min.css' => [
+        'version' => '1.9.1',
+        'type' => 'css',
+    ],
+    'stimulus-flatpickr' => [
+        'version' => '1.4.0',
+    ],
+    'stimulus' => [
+        'version' => '1.1.1',
+    ],
+    'flatpickr' => [
+        'version' => '4.6.6',
+    ],
+    '@stimulus/core' => [
+        'version' => '1.1.1',
+    ],
+    'flatpickr/dist/flatpickr.min.css' => [
+        'version' => '4.6.6',
+        'type' => 'css',
+    ],
+    '@stimulus/mutation-observers' => [
+        'version' => '1.1.1',
+    ],
+    '@stimulus/multimap' => [
+        'version' => '2.0.0',
+    ],
+    'choices.js' => [
+        'version' => '11.1.0',
+    ],
+    'choices.js/public/assets/styles/choices.min.css' => [
+        'version' => '11.1.0',
         'type' => 'css',
     ],
 ];
