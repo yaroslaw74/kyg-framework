76d4fb834eff901eb33b85f9a953306e3d734f3f
Ярослав <yaroslaw74@gmail.com>
2026-01-26 18:15:53 +0200

    Create SonataClassificationTag.php

---
+++ modules/Classification/Entity/SonataClassificationTag.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Classification\Repository\SonataClassificationTagRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\ClassificationBundle\Entity\BaseTag;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: SonataClassificationTagRepository::class)]
+#[ORM\Table(name: 'classification__tag')]
+#[ApiResource]
+class SonataClassificationTag extends BaseTag
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    protected ?Uuid $id = null;
+
+    public function getId(): ?string
+    {
+        if (null !== $this->id) {
+            return $this->id->toString();
+        }
+
+        return null;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+}
