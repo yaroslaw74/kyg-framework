b37330a811f1eb5bb2d646dc091886b4f8d01fc8
Ярослав <yaroslaw74@gmail.com>
2026-01-26 18:15:57 +0200

    Create SonataClassificationCollectionRepository.php

---
+++ modules/Classification/Repository/SonataClassificationCollectionRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Repository;
+
+use App\Modules\Classification\Entity\SonataClassificationCollection;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<SonataClassificationCollection>
+ */
+class SonataClassificationCollectionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SonataClassificationCollection::class);
+    }
+
+    /**
+     * @return SonataClassificationCollection[] Returns an array of SonataClassificationCollection objects
+     */
+    public function findByExampleField($value): array
+    {
+        /* @phpstan-ignore  doctrine.dql */
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * Summary of findOneBySomeField.
+     */
+    public function findOneBySomeField($value): ?SonataClassificationCollection
+    {
+        /* @phpstan-ignore  doctrine.dql */
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
