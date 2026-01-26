68928d8f1baeb230d0f2755db97c2b84f34930fb
Ярослав <yaroslaw74@gmail.com>
2026-01-26 18:16:23 +0200

    Create SonataClassificationCategory.php

---
+++ modules/Classification/Entity/SonataClassificationCategory.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Classification\Repository\SonataClassificationCategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\ClassificationBundle\Entity\BaseCategory;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: SonataClassificationCategoryRepository::class)]
+#[ORM\Table(name: 'classification__category')]
+#[ApiResource]
+class SonataClassificationCategory extends BaseCategory
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    protected ?Uuid $id = null;
+
+    public function getId(): ?string
+    {
+        if (null !== $this->id) {
+            return $this->id->toString();
+        }
+
+        return null;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+}
