727cae2696496fb7e773c9ce1cccf51150afe694
Ярослав <yaroslaw74@gmail.com>
2026-01-27 13:25:34 +0200

    Update SonataClassificationContextRepository.php

--- modules/Classification/Repository/SonataClassificationContextRepository.php
+++ modules/Classification/Repository/SonataClassificationContextRepository.php
@@ -35,6 +35,9 @@ class SonataClassificationContextRepository extends ServiceEntityRepository
      */
     public function findByExampleField($value): array
     {
+        /*
+         * @phpstan-ignore doctrine.dql
+         */
         return $this->createQueryBuilder('s')
             ->andWhere('s.exampleField = :val')
             ->setParameter('val', $value)
@@ -50,6 +53,9 @@ class SonataClassificationContextRepository extends ServiceEntityRepository
      */
     public function findOneBySomeField($value): ?SonataClassificationContext
     {
+        /*
+         * @phpstan-ignore doctrine.dql
+         */
         return $this->createQueryBuilder('s')
             ->andWhere('s.exampleField = :val')
             ->setParameter('val', $value)
