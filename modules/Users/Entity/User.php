d9021b15d351e0b84641e70ce7b3aa8748d6603b
Ярослав <yaroslaw74@gmail.com>
2026-01-27 22:57:29 +0200

    Update User.php

--- modules/Users/Entity/User.php
+++ modules/Users/Entity/User.php
@@ -23,18 +23,71 @@ use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
+use Sonata\IntlBundle\Timezone\TimezoneAwareInterface;
+use Sonata\IntlBundle\Timezone\TimezoneAwareTrait;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: 'user__user')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+#[Gedmo\SoftDeleteable]
 #[ApiResource]
-class User extends BaseUser implements UserInterface, PasswordAuthenticatedUserInterface
+class User extends BaseUser implements UserInterface, PasswordAuthenticatedUserInterface, TimezoneAwareInterface
 {
+    use TimezoneAwareTrait;
+    use SoftDeleteableEntity;
+
     #[ORM\Id]
     #[ORM\Column(type: UuidType::NAME, unique: true)]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
     protected $id;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $first_name = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $last_name = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $middle_name = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $locale = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $avatar = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $gravatar = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $facebook = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $google = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $yandex = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $linkedin = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $mailru = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $odnoklassniki = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $vkontakte = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $x_twitter = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $github = null;
 
     /**
      * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
@@ -42,11 +95,14 @@ class User extends BaseUser implements UserInterface, PasswordAuthenticatedUserI
     public function __serialize(): array
     {
         $data = (array) $this;
-        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+        $data["\0" . self::class . "\0password"] = hash('crc32c', $this->password);
 
         return $data;
     }
 
+    /**
+     * @param mixed[] $data
+     */
     public function __unserialize(array $data): void
     {
         [
@@ -57,14 +113,31 @@ class User extends BaseUser implements UserInterface, PasswordAuthenticatedUserI
             $this->emailCanonical,
             $this->password,
             $this->plainPassword,
+            $this->first_name,
+            $this->last_name,
+            $this->middle_name,
+            $this->locale,
+            $this->avatar,
+            $this->gravatar,
             $this->enabled,
             $this->salt,
             $this->lastLogin,
             $this->confirmationToken,
             $this->passwordRequestedAt,
             $this->roles,
+            $this->facebook,
+            $this->google,
+            $this->yandex,
+            $this->linkedin,
+            $this->mailru,
+            $this->odnoklassniki,
+            $this->x_twitter,
+            $this->vkontakte,
+            $this->github,
             $this->createdAt,
             $this->updatedAt,
+            $this->deletedAt,
+            $this->timezone,
         ] = $data;
     }
 
@@ -86,5 +159,192 @@ class User extends BaseUser implements UserInterface, PasswordAuthenticatedUserI
          * @phpstan-ignore return.type
          */
         return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(?string $first_name): static
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(?string $last_name): static
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    public function getMiddleName(): ?string
+    {
+        return $this->middle_name;
+    }
+
+    public function setMiddleName(?string $middle_name): static
+    {
+        $this->middle_name = $middle_name;
+
+        return $this;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(?string $locale): static
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(?string $avatar): static
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    public function getGravatar(): ?string
+    {
+        return $this->gravatar;
+    }
+
+    public function setGravatar(?string $gravatar): static
+    {
+        $this->gravatar = $gravatar;
+
+        return $this;
+    }
+
+    public function getFacebook(): ?string
+    {
+        return $this->facebook;
+    }
+
+    public function setFacebook(?string $facebook): static
+    {
+        $this->facebook = $facebook;
+
+        return $this;
+    }
+
+    public function getGoogle(): ?string
+    {
+        return $this->google;
+    }
+
+    public function setGoogle(?string $google): static
+    {
+        $this->google = $google;
+
+        return $this;
+    }
+
+    public function getYandex(): ?string
+    {
+        return $this->yandex;
+    }
+
+    public function setYandex(?string $yandex): static
+    {
+        $this->yandex = $yandex;
+
+        return $this;
+    }
+
+    public function getLinkedin(): ?string
+    {
+        return $this->linkedin;
+    }
+
+    public function setLinkedin(?string $linkedin): static
+    {
+        $this->linkedin = $linkedin;
+
+        return $this;
+    }
+
+    public function getMailru(): ?string
+    {
+        return $this->mailru;
+    }
+
+    public function setMailru(?string $mailru): static
+    {
+        $this->mailru = $mailru;
+
+        return $this;
+    }
+
+    public function getOdnoklassniki(): ?string
+    {
+        return $this->odnoklassniki;
+    }
+
+    public function setOdnoklassniki(?string $odnoklassniki): static
+    {
+        $this->odnoklassniki = $odnoklassniki;
+
+        return $this;
+    }
+
+    public function getVkontakte(): ?string
+    {
+        return $this->vkontakte;
+    }
+
+    public function setVkontakte(?string $vkontakte): static
+    {
+        $this->vkontakte = $vkontakte;
+
+        return $this;
+    }
+
+    public function getXTwitter(): ?string
+    {
+        return $this->x_twitter;
+    }
+
+    public function setXTwitter(?string $x_twitter): static
+    {
+        $this->x_twitter = $x_twitter;
+
+        return $this;
+    }
+
+    public function getGithub(): ?string
+    {
+        return $this->github;
+    }
+
+    public function setGithub(?string $github): static
+    {
+        $this->github = $github;
+
+        return $this;
+    }
+
+    public function setTimezone(string $timezone): static
+    {
+        $this->timezone = $timezone;
+
+        return $this;
     }
 }
