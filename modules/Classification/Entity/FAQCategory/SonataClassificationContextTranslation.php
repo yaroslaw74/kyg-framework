5ba6b6ad1eaf9426d4c0d32cbe7ef707d76c13e5
Ярослав <yaroslaw74@gmail.com>
2026-01-28 20:32:59 +0200

    Create SonataClassificationContextTranslation.php

---
+++ modules/Classification/Entity/FAQCategory/SonataClassificationContextTranslation.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity\FAQCategory;
+
+use App\Modules\Classification\Entity\SonataClassificationContext;
+use App\Modules\Classification\Repository\FAQCategory\SonataClassificationContextTranslationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+#[ORM\Entity(repositoryClass: SonataClassificationContextTranslationRepository::class)]
+#[ORM\Table(name: 'classification__context_translations')]
+#[ORM\UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_id', 'field'])]
+class SonataClassificationContextTranslation extends AbstractPersonalTranslation
+{
+    #[ORM\ManyToOne(targetEntity: SonataClassificationContext::class, inversedBy: 'translations')]
+    #[ORM\JoinColumn(name: 'object_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    protected ?SonataClassificationContext $objects = null;
+
+    /**
+     * Convenient constructor.
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale, $field, $value)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->locale,
+            $this->field,
+            $this->object,
+            $this->content,
+        ] = $data;
+    }
+}
