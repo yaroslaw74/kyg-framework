664913535000692862abf9528536adf29282224c
Ярослав <yaroslaw74@gmail.com>
2026-01-28 21:13:24 +0200

    Create ContextAdmin.php

---
+++ modules/Classification/Admin/ContextAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Admin
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Admin;
+
+use App\Modules\Classification\Entity\SonataClassificationContext;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\TranslationBundle\Enum\TranslationFilterMode;
+use Sonata\TranslationBundle\Filter\TranslationFieldFilter;
+
+/**
+ * @extends AbstractAdmin<SonataClassificationContext>
+ */
+final class ContextAdmin extends AbstractAdmin
+{
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+    {
+        $datagridMapper
+            ->add('title', TranslationFieldFilter::class, [
+                'filter_mode' => TranslationFilterMode::GEDMO,
+            ]);
+    }
+}
