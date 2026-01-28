0c77b54db8a84d97bef3e6380da43634ae87ae83
Ярослав <yaroslaw74@gmail.com>
2026-01-28 21:13:31 +0200

    Update SonataClassificationCategory.php

--- modules/Classification/Entity/SonataClassificationCategory.php
+++ modules/Classification/Entity/SonataClassificationCategory.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace App\Modules\Classification\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use App\Modules\Classification\Entity\FAQCategory\SonataClassificationCategoryTranslation;
 use App\Modules\Classification\Repository\SonataClassificationCategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
