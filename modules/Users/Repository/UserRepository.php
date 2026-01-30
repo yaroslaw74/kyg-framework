1e2780934afcc18ddab5d49b0a722aac1ed934a1
Ярослав <yaroslaw74@gmail.com>
2026-01-30 21:32:59 +0200

    Update UserRepository.php

--- modules/Users/Repository/UserRepository.php
+++ modules/Users/Repository/UserRepository.php
@@ -21,7 +21,6 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
-
 /**
  * @extends ServiceEntityRepository<User>
  */
