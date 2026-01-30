ace774c772daf780826170016e3a57428fc4bf02
Ярослав <yaroslaw74@gmail.com>
2026-01-30 20:49:38 +0200

    Delete RuntimeException.php

--- modules/Config/Exception/RuntimeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Account\Exception;
-
-use App\Account\Contract\Exception\ExceptionInterface;
-
-class RuntimeException extends \RuntimeException implements ExceptionInterface
-{
-}
