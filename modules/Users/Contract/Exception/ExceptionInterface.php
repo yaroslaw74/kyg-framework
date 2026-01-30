d8b0b48bf50dabfa264fb363b78e71b26ac90816
Ярослав <yaroslaw74@gmail.com>
2026-01-30 20:46:24 +0200

    Create ExceptionInterface.php

---
+++ modules/Users/Contract/Exception/ExceptionInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Contract Exception
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+namespace App\Modules\Users\Contract\Exception;
+
+interface ExceptionInterface extends \Throwable
+{
+}
