b5317c2888ac237c8858ac3d759c663cfbe27bf8
Ярослав <yaroslaw74@gmail.com>
2026-02-01 12:33:39 +0200

    Create DoctrineVar.php

---
+++ src/DependencyInjection/DoctrineVar.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Dependency Injection
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\DependencyInjection;
+
+use App\Settings\DoctrineSettings;
+use Jbtronics\SettingsBundle\Manager\SettingsManagerInterface;
+use Symfony\Component\DependencyInjection\EnvVarLoaderInterface;
+
+final class DoctrineVar implements EnvVarLoaderInterface
+{
+    public function __construct(private SettingsManagerInterface $settingsManager)
+    {
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function loadEnvVars(): array
+    {
+        $settings = $this->settingsManager->get(DoctrineSettings::class);
+
+        return [
+            'DATABASE_URL' => $settings->getDbalUrl(),
+        ];
+    }
+}
