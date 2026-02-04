92c4e92d6aa7b2c73f92869535acf17b7313858f
Ярослав <yaroslaw74@gmail.com>
2026-02-04 11:40:54 +0200

    Create SonataMediaGallery.php

---
+++ modules/Media/Entity/SonataMediaGallery.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Media\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Media\Repository\SonataMediaGalleryRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Sonata\MediaBundle\Entity\BaseGallery;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @phpstan-ignore missingType.generics
+ */
+#[ORM\Entity(repositoryClass: SonataMediaGalleryRepository::class)]
+#[ORM\Table(name: 'media__gallery')]
+#[Gedmo\SoftDeleteable]
+#[ApiResource]
+class SonataMediaGallery extends BaseGallery
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    protected ?Uuid $id = null;
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->context,
+            $this->name,
+            $this->enabled,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->createdAt,
+            $this->createdBy,
+            $this->deletedAt,
+            $this->defaultFormat,
+            $this->galleryItems,
+        ] = $data;
+    }
+
+    public function getId(): ?string
+    {
+        if (null !== $this->id) {
+            return $this->id->toString();
+        }
+
+        return null;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+}
