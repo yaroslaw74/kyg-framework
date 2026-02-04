833d53da083980be192cc33367bc245f3d5b0952
Ярослав <yaroslaw74@gmail.com>
2026-02-04 10:28:46 +0200

    Create SonataClassificationContext.php

---
+++ modules/Classification/Entity/SonataClassificationContext.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Classification\Repository\SonataClassificationContextRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Sonata\ClassificationBundle\Entity\BaseContext;
+
+#[ORM\Entity(repositoryClass: SonataClassificationContextRepository::class)]
+#[ORM\Table(name: 'classification__context')]
+#[Gedmo\SoftDeleteable]
+#[ApiResource]
+class SonataClassificationContext extends BaseContext
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+
+    #[ORM\Id]
+    #[ORM\Column(type: Types::STRING)]
+    /**
+     * @phpstan-ignore doctrine.columnType
+     */
+    protected ?string $id = null;
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->name,
+            $this->createdAt,
+            $this->createdBy,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->deletedAt,
+            $this->enabled,
+        ] = $data;
+    }
+}
