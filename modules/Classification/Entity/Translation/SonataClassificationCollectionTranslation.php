0e76750bd376e565bd776825aea94ff5ec94cb08
Ярослав <yaroslaw74@gmail.com>
2026-02-04 21:05:40 +0200

    Create SonataClassificationCollectionTranslation.php

---
+++ modules/Classification/Entity/Translation/SonataClassificationCollectionTranslation.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity\Translation;
+
+use App\Modules\Classification\Entity\SonataClassificationCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+use Gedmo\Translatable\Entity\Repository\TranslationRepository;
+
+#[ORM\Table(name: 'classification__collection_translation')]
+#[ORM\Index(name: 'classification__collection_translation_idx', columns: ['foreign_key', 'locale', 'object_class', 'field'])]
+#[ORM\Entity(repositoryClass: TranslationRepository::class)]
+class SonataClassificationCollectionTranslation extends AbstractPersonalTranslation
+{
+    #[ORM\ManyToOne(targetEntity: SonataClassificationCollection::class, inversedBy: 'translations')]
+    #[ORM\JoinColumn(name: 'object_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    protected $object;
+
+    /**
+     * Convenient constructor.
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale, $field, $value)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+}
