45260869c81a2117c9316579b8303278c5609a9c
Ярослав <yaroslaw74@gmail.com>
2026-02-04 11:40:57 +0200

    Create SonataMediaGalleryItem.php

---
+++ modules/Media/Entity/SonataMediaGalleryItem.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Media\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Media\Repository\SonataMediaGalleryItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Sonata\MediaBundle\Entity\BaseGalleryItem;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: SonataMediaGalleryItemRepository::class)]
+#[ORM\Table(name: 'media__gallery_item')]
+#[Gedmo\SoftDeleteable]
+#[ApiResource]
+class SonataMediaGalleryItem extends BaseGalleryItem
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    protected ?Uuid $id = null;
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->media,
+            $this->gallery,
+            $this->position,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->createdAt,
+            $this->createdBy,
+            $this->deletedAt,
+            $this->enabled,
+        ] = $data;
+    }
+
+    public function getId(): ?string
+    {
+        if (null !== $this->id) {
+            return $this->id->toString();
+        }
+
+        return null;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+}
