4bfb97c33c2bb02dbbcc480791eb78b3b425b17f
Ярослав <yaroslaw74@gmail.com>
2026-02-06 18:27:16 +0200

    Update importmap.php

--- importmap.php
+++ importmap.php
@@ -3,13 +3,13 @@
 /**
  * Returns the importmap for this application.
  *
- * - 'path' is a path inside the asset mapper system. Use the
- *     'debug:asset-map' command to see the full list of paths.
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
  *
- * - 'entrypoint' (JavaScript only) set to true for any module that will
- *     be used as an 'entrypoint' (and passed to the importmap() Twig function).
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
  *
- * The 'importmap:require' command can be used to add new entries to this file.
+ * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
     'app' => [
@@ -29,6 +29,9 @@ return [
     ],
     '@symfony/ux-live-component' => [
         'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
+    '@symfony/ux-google-map' => [
+        'path' => './vendor/symfony/ux-google-map/assets/dist/map_controller.js',
     ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
