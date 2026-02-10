68b0e8aae659f0035dc0b79e3dc0bddef847c2fe
Ярослав <yaroslaw74@gmail.com>
2026-02-10 20:46:30 +0200

    Update scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,14 @@
-document.addEventListener("click", function (event) {
+document.addEventListener("click", (event) => {
     if (event.target.classList.contains("nav-link")) {
-        let nav_linc = document.querySelectorAll(".nav-link");
-        nav_linc.forEach((el) => el.classList.remove("active"));
+        const NAV_LINC = document.querySelectorAll(".nav-link");
+        NAV_LINC.forEach((el) => {
+            el.classList.remove("active");
+        });
         event.target.classList.add("active");
 
         let id = event.target.getAttribute("id");
 
-        id = "target_" + id;
+        id = `target_${id}`;
 
         const ELEMENT = document.getElementById(id);
         if (ELEMENT) {
