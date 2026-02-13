f208f7e9162b66dffe6b931d91631a35f224ae02
Ярослав <yaroslaw74@gmail.com>
2026-02-13 22:17:36 +0200

    Create FileManagerTwigExtension.php

---
+++ modules/Administration/Twig/FileManagerTwigExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Twig Extension
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Administration\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\ExtensionInterface;
+use Twig\TwigFunction;
+use App\Modules\Administration\Service\FileManagerServise;
+use Twig\Extension\GlobalsInterface;
+
+class FileManagerTwigExtension extends AbstractExtension implements ExtensionInterface, GlobalsInterface
+{
+    public function __construct(private FileManagerServise $fileManagerService)
+    {
+    }
+
+    public function sizeDirExtension(string $path): string
+    {
+        return $this->fileManagerService->getDirSize($path);
+    }
+
+    /**
+     * @return TwigFunction[]
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('size_dir', [$this, 'sizeDirExtension'])
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getGlobals(): array
+    {
+        return [
+            'storage_dir' => $this->fileManagerService->getStoragePath(),
+            'storage_free_space' => $this->fileManagerService->formatBytes($this->fileManagerService->getFreeSpaseStorage()),
+            'storage_size_percent' => $this->fileManagerService->getDirSizePercent($this->fileManagerService->getStoragePath())
+        ];
+    }
+}
