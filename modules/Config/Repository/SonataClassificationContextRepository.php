c58d23c804e14e47541d745b974e759048efa02d
Ярослав <yaroslaw74@gmail.com>
2026-02-19 22:11:44 +0200

    Create SonataClassificationContextRepository.php

---
+++ modules/Config/Repository/SonataClassificationContextRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Config\Repository;
+
+use App\Modules\Config\Entity\SonataClassificationContext;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<SonataClassificationContext>
+ */
+class SonataClassificationContextRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SonataClassificationContext::class);
+    }
+
+    /**
+     * @return SonataClassificationContext[] Returns an array of SonataClassificationContext objects
+     *
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findByExampleField($value): array
+    {
+        /*
+         * @phpstan-ignore doctrine.dql
+         */
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findOneBySomeField($value): ?SonataClassificationContext
+    {
+        /*
+         * @phpstan-ignore doctrine.dql
+         */
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
