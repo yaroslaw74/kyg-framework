582092508acf020a7288fa0902e02fa19c55e0d8
Ярослав <yaroslaw74@gmail.com>
2026-02-19 20:30:48 +0200

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -69,4 +69,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Nucleos\SonataAutoConfigureBundle\SonataAutoConfigureBundle::class => ['all' => true],
+    Sonata\MediaBundle\SonataMediaBundle::class => ['all' => true],
 ];
