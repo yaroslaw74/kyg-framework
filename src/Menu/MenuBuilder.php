1a33047f9c4cb076e7e12feb68cb72bb5f7c0fbe
Ярослав <yaroslaw74@gmail.com>
2026-02-22 20:31:08 +0200

    Create MenuBuilder.php

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Menu Builder
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Knp\Menu\Attribute\AsMenuBuilder;
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+
+class MenuBuilder
+{
+    public function __construct(private FactoryInterface $factory)
+    {
+    }
+    #[AsMenuBuilder('app_sidebar_menu')]
+    public function mainMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('app_sidebar_menu');
+        $menu->setChildrenAttribute('class', 'main-menu');
+
+        $menu->addChild('home', [
+            'label' => 'Home',
+            'attributes' => [
+                'class' => 'slide__category'
+            ],
+            'labelAttributes' => [
+                'class' => 'category-name'
+            ]
+        ]);
+        $menu->addChild('dashboard', [
+            'route' => 'app_home',
+            'label' => 'Dashboard',
+            'attributes' => [
+                'class' => 'slide'
+            ],
+            'labelAttributes' => [
+                'class' => 'side-menu__label'
+            ],
+            'linkAttributes' => [
+                'class' => 'side-menu__item'
+            ],
+            'extras' => [
+                'icon' => 'ic:twotone-dashboard'
+            ]
+        ]);
+
+        return $menu;
+    }
+}
