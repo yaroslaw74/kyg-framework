1904f204123d7323bb0286a58c78a261e013e6cf
Ярослав <yaroslaw74@gmail.com>
2026-02-23 09:56:10 +0200

    Update stimulus_bootstrap.js

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,11 +1,9 @@
 import { startStimulusApp } from "@symfony/stimulus-bundle";
-import Swup from "@symfony/ux-swup";
 import toglePassword from "@symfony/ux-toggle-password";
 import Flatpickr from "stimulus-flatpickr";
 
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
-app.register("swup", Swup);
 app.register("toggle-password", toglePassword);
 app.register("flatpickr", Flatpickr);
