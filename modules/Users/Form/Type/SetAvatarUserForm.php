7844e7c9d3c6c277ca3bd450ae18659dee5d34b9
Ярослав <yaroslaw74@gmail.com>
2026-02-23 21:35:27 +0200

    Create SetAvatarUserForm.php

---
+++ modules/Users/Form/Type/SetAvatarUserForm.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Form Type
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Users\Form\Type;
+
+use App\Modules\Users\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\UX\Dropzone\Form\DropzoneType;
+
+/**
+ * @template-extends AbstractType<User>
+ */
+class SetAvatarUserForm extends AbstractType
+{
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('avatar', DropzoneType::class, [
+            'label' => $this->translator->trans('Avatar', [], 'users'),
+            'attr' => [
+                'placeholder' => $this->translator->trans('Drag and drop or browse image', [], 'users'),
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
