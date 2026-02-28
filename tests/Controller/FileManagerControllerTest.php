641b277ea3a4e3b2acfaa58ba68c5a966b24cc11
Ярослав <yaroslaw74@gmail.com>
2026-02-28 20:55:13 +0200

    Create FileManagerControllerTest.php

---
+++ tests/Controller/FileManagerControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   test Controller
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class FileManagerControllerTest extends WebTestCase
+{
+    public function testFileManager(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/app/admin/file/manager');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testFileManagerList(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/app/admin/file/manager/list/{list}');
+
+        self::assertResponseIsSuccessful();
+    }
+}
