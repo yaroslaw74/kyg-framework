0ca7e6a05f852ca9e2533a58da12a60726cf2009
Ярослав <yaroslaw74@gmail.com>
2026-03-01 19:09:16 +0200

    Create NomenclatureRepository.php

---
+++ modules/Nomenclature/Repository/NomenclatureRepository.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+namespace App\Modules\Nomenclature\Repository;
+
+use App\Modules\Nomenclature\Entity\Nomenclature;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+
+/**
+ * @extends ServiceEntityRepository<Nomenclature>
+ */
+class NomenclatureRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Nomenclature::class);
+    }
+
+    /**
+     * @return Nomenclature[] Returns an array of Nomenclature objects
+     *
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findByExampleField($value): array
+    {
+        /* @phpstan-ignore doctrine.dql */
+        return $this->createQueryBuilder('n')
+            ->andWhere('n.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('n.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findOneBySomeField($value): ?Nomenclature
+    {
+        /* @phpstan-ignore doctrine.dql */
+        return $this->createQueryBuilder('n')
+            ->andWhere('n.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
