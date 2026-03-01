8881061f06a35c2621a09067b80f5b97a371293b
Ярослав <yaroslaw74@gmail.com>
2026-03-01 17:05:25 +0200

    Create ClassificationCollectionRepository.php

---
+++ modules/Classification/Repository/ClassificationCollectionRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Repository;
+
+use App\Modules\Classification\Entity\ClassificationCollection;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<ClassificationCollection>
+ */
+class ClassificationCollectionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ClassificationCollection::class);
+    }
+
+    /**
+     * @return ClassificationCollection[] Returns an array of ClassificationCollection objects
+     *
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findByExampleField($value): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findOneBySomeField($value): ?ClassificationCollection
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
