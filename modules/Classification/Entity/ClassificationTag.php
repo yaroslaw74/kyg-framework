0cd23f1c4c3a74c19d80a9a0a8edef6feb41b3ec
Ярослав <yaroslaw74@gmail.com>
2026-03-01 17:05:20 +0200

    Create ClassificationTag.php

---
+++ modules/Classification/Entity/ClassificationTag.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Classification\Repository\ClassificationTagRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: ClassificationTagRepository::class)]
+#[ORM\Table(name: 'classification__tag')]
+#[Gedmo\SoftDeleteable]
+#[ApiResource]
+class ClassificationTag
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    #[Gedmo\Slug(fields: ['name'])]
+    private ?string $slug = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    protected ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    protected ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'tags')]
+    private ?ClassificationContext $context = null;
+
+    public function __toString(): string
+    {
+        return $this->getName() ?? 'n/a';
+    }
+
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->name,
+            $this->slug,
+            $this->createdAt,
+            $this->createdBy,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->deletedAt,
+            $this->context,
+        ] = $data;
+    }
+
+    public function getId(): ?string
+    {
+        if (null !== $this->id) {
+            return $this->id->toString();
+        }
+
+        return null;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function prePersist(): void
+    {
+        $this->setCreatedAt(new \DateTime());
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+    public function preUpdate(): void
+    {
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function getContext(): ?ClassificationContext
+    {
+        return $this->context;
+    }
+
+    public function setContext(?ClassificationContext $context): static
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+}
