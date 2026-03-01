c0916937c4fb348d1aac5c9f7c3fe926b23a939d
Ярослав <yaroslaw74@gmail.com>
2026-03-01 13:27:43 +0200

    Update FileManagerController.php

--- modules/System/Controller/FileManagerController.php
+++ modules/System/Controller/FileManagerController.php
@@ -14,8 +14,11 @@ declare(strict_types=1);
 
 namespace App\Modules\System\Controller;
 
+use App\Modules\System\Entity\Files;
 use App\Modules\System\Service\FileManagerServise;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
+use Rekalogika\Contracts\File\Metadata\ImageMetadataInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,6 +29,7 @@ final class FileManagerController extends AbstractController
     public function __construct(
         private readonly FileManagerServise $fileManagerService,
         private PaginatorInterface $paginator,
+        private EntityManagerInterface $entityManager,
     ) {
     }
 
@@ -53,6 +57,56 @@ final class FileManagerController extends AbstractController
         return $this->render('@System/file_manager/file-manager-list.html.twig', [
             'pagination' => $pagination,
             'list' => $list,
+        ]);
+    }
+
+    #[Route('/app/admin/file/manager/detalis/{path}/{name}', name: 'app_admin_file_manager_detalis')]
+    public function fileDetails(Request $request, ?string $path = null, ?string $name = null): Response
+    {
+        $repository = $this->entityManager->getRepository(Files::class);
+        $files = $repository->findBy(['path' => $path]);
+        $id = null;
+        if (null != $files) {
+            foreach ($files as $value) {
+                if ((string) $value->getName() === $name) {
+                    $id = $value->getId();
+                }
+            }
+        }
+        if (null == $id) {
+            return $this->render('@System/file_manager/file-manager-details.html.twig', [
+                'file' => [
+                    'path' => $path,
+                    'name' => $name,
+                    'size' => $this->fileManagerService->formatBytes(filesize($this->fileManagerService->getStoragePath().'/{$path}/{$name}')),
+                    'createdAt' => '',
+                    'createdBy' => '',
+                    'updatedAt' => '',
+                    'updatedBy' => '',
+                    'width' => '',
+                    'height' => '',
+                    'format' => mime_content_type($this->fileManagerService->getStoragePath().'/{$path}/{$name}'),
+                    'type' => finfo_file(finfo_open(FILEINFO_MIME_ENCODING), $this->fileManagerService->getStoragePath().'/{$path}/{$name}'),
+                ],
+            ]);
+        }
+
+        $file = $repository->find($id);
+
+        return $this->render('@System/file_manager/file-manager-details.html.twig', [
+            'file' => [
+                'path' => $file->getPath(),
+                'name' => (string) $file->getName(),
+                'size' => $this->fileManagerService->formatBytes($file->getSize()),
+                'createdAt' => $file->getCreatedAt(),
+                'createdBy' => $file->getCreatedBy(),
+                'updatedAt' => $file->getUpdatedAt(),
+                'updatedBy' => $file->getUpdatedBy(),
+                'width' => $file->get(ImageMetadataInterface::class)?->getWidth(),
+                'height' => $file->get(ImageMetadataInterface::class)?->getHeight(),
+                'format' => $file->getType()->getType(),
+                'type' => $file->getType()->getName(),
+            ],
         ]);
     }
 }
