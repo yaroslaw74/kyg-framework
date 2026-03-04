9fed03b9510e4aefa0d9be987a8f68dd7257f49e
Ярослав <yaroslaw74@gmail.com>
2026-03-04 21:09:00 +0200

    Create NomenclatureTranslation.php

---
+++ modules/Nomenclature/Entity/Translation/NomenclatureTranslation.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Nomenclature\Entity\Translation;
+
+use App\Modules\Nomenclature\Entity\Nomenclature;
+use App\Modules\Nomenclature\Repository\Translation\NomenclatureTranslationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+#[ORM\Entity(repositoryClass: NomenclatureTranslationRepository::class)]
+#[ORM\Table(name: 'nomenclature__nomenclature_translation')]
+#[ORM\UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_id', 'field'])]
+class NomenclatureTranslation extends AbstractPersonalTranslation
+{
+    #[ORM\ManyToOne(targetEntity: Nomenclature::class, inversedBy: 'translations')]
+    #[ORM\JoinColumn(name: 'object_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    protected $object;
+
+    /**
+     * Convenient constructor.
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale, $field, $value)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+}
