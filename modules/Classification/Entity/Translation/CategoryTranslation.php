272447678dbfa1014e34eec156f639b2df9e4f66
Ярослав <yaroslaw74@gmail.com>
2026-03-04 21:08:44 +0200

    Create CategoryTranslation.php

---
+++ modules/Classification/Entity/Translation/CategoryTranslation.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Classification\Entity\Translation;
+
+use App\Modules\Classification\Entity\ClassificationCategory;
+use App\Modules\Classification\Repository\Translation\CategoryTranslationRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\MappedSuperclass\AbstractPersonalTranslation;
+
+#[ORM\Entity(repositoryClass: CategoryTranslationRepository::class)]
+#[ORM\Table(name: 'classification__category_translation')]
+#[ORM\UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_id', 'field'])]
+class CategoryTranslation extends AbstractPersonalTranslation
+{
+    #[ORM\ManyToOne(targetEntity: ClassificationCategory::class, inversedBy: 'translations')]
+    #[ORM\JoinColumn(name: 'object_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    protected $object;
+
+    /**
+     * Convenient constructor.
+     *
+     * @param string $locale
+     * @param string $field
+     * @param string $value
+     */
+    public function __construct($locale, $field, $value)
+    {
+        $this->setLocale($locale);
+        $this->setField($field);
+        $this->setContent($value);
+    }
+}
