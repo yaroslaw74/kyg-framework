77d749ce78162bfa0377de1305432aa46cdf8cd4
Ярослав <yaroslaw74@gmail.com>
2026-03-05 21:35:56 +0200

    Update HomeControllerTest.php

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -16,9 +16,8 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class AppControllerTest extends WebTestCase
+final class HomeControllerTest extends WebTestCase
 {
-
     public function testIndex(): void
     {
         $client = static::createClient();
