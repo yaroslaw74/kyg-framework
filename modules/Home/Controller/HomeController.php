0783e1127a6d568da023455925ab567326d3e0f3
Ярослав <yaroslaw74@gmail.com>
2026-03-05 19:44:53 +0200

    Create HomeController.php

---
+++ modules/Home/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Controller
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Home\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+
+    #[Route('/app/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('@Home/app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+}
