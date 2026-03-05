163212efc5921adefa2cb9f0dbba4733428006e5
Ярослав <yaroslaw74@gmail.com>
2026-03-05 21:35:32 +0200

    Update HomeController.php

--- modules/Home/Controller/HomeController.php
+++ modules/Home/Controller/HomeController.php
@@ -20,7 +20,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-
     #[Route('/app/home', name: 'app_home')]
     public function index(): Response
     {
