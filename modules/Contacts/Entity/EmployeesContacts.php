fce0d36b609c0d9ad7c6a1b5bb0ff7455f12c3af
Ярослав <yaroslaw74@gmail.com>
2026-03-14 20:18:17 +0200

    Create EmployeesContacts.php

---
+++ modules/Contacts/Entity/EmployeesContacts.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Contacts\Entity;
+
+use App\Modules\Contacts\Repository\EmployeesContactsRepository;
+use App\Modules\Employees\Entity\Employees;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: EmployeesContactsRepository::class)]
+#[ORM\Table(name: 'employees__contacts')]
+#[Gedmo\SoftDeleteable]
+class EmployeesContacts
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'contacts')]
+    private ?Employees $employee = null;
+
+    #[ORM\ManyToOne(inversedBy: 'employees')]
+    private ?Contacts $name = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $value = null;
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->createdAt,
+            $this->createdBy,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->deletedAt,
+            $this->employee,
+            $this->value,
+        ] = $data;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmployee(): ?Employees
+    {
+        return $this->employee;
+    }
+
+    public function setEmployee(?Employees $employee): static
+    {
+        $this->employee = $employee;
+
+        return $this;
+    }
+
+    public function getName(): ?Contacts
+    {
+        return $this->name;
+    }
+
+    public function setName(?Contacts $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(?string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
