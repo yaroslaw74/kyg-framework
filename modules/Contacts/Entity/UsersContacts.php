885621ce546535165c49ea754b34e4fb629e657f
Ярослав <yaroslaw74@gmail.com>
2026-03-14 20:18:28 +0200

    Create UsersContacts.php

---
+++ modules/Contacts/Entity/UsersContacts.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Contacts\Entity;
+
+use App\Modules\Contacts\Repository\UsersContactsRepository;
+use App\Modules\Users\Entity\User;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: UsersContactsRepository::class)]
+#[ORM\Table(name: 'user__contacts')]
+#[Gedmo\SoftDeleteable]
+class UsersContacts
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'contacts')]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'users')]
+    private ?Contacts $name = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $value = null;
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->createdAt,
+            $this->createdBy,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->deletedAt,
+            $this->user,
+            $this->name,
+            $this->value,
+        ] = $data;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getName(): ?Contacts
+    {
+        return $this->name;
+    }
+
+    public function setName(?Contacts $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(?string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
