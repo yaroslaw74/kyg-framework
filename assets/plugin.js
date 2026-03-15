ec720c94a1b94467469441090ff0696d73d8e093
Ярослав <yaroslaw74@gmail.com>
2026-03-15 10:57:26 +0200

    Create plugin.js

---
+++ assets/plugin.js
@@ -0,0 +1,35 @@
+/*
+ * Apex Charts
+ */
+import "./vendor/apexcharts/apexcharts.index.js";
+import "./vendor/apexcharts/dist/apexcharts.css";
+
+/*
+ * Choices JS
+ */
+import "./vendor/choices.js/choices.js.index.js";
+import "./vendor/choices.js/public/assets/styles/choices.min.css";
+
+/*
+ * Color Picker
+ */
+import "./vendor/@simonwep/pickr/dist/pickr.es5.min.js";
+import "./vendor/@simonwep/pickr/dist/themes/nano.min.css";
+
+/*
+ * Flatpickr - javascript datetime picker
+ */
+import "./vendor/flatpickr/flatpickr.index.js";
+import "./vendor/flatpickr/dist/flatpickr.min.css";
+
+/*
+ * Node Waves
+ */
+import "./vendor/node-waves/node-waves.index.js";
+import "./vendor/node-waves/dist/waves.min.css";
+
+/*
+ * Simplebar
+ */
+import "./vendor/simplebar/simplebar.index.js";
+import "./vendor/simplebar/dist/simplebar.min.css";
