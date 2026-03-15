b43cd9382e636f61dd25c82543642432d7fe9a11
Ярослав <yaroslaw74@gmail.com>
2026-03-15 10:57:53 +0200

    Create ChartsInterface.php

---
+++ modules/Charts/Extension/ChartsInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Charts Extension Interface
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Charts\Extension;
+
+interface ChartsInterface
+{
+}
