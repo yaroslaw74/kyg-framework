d1ce01b8764a437e0d819ec8a176f22dec8b30be
Ярослав <yaroslaw74@gmail.com>
2026-03-25 20:35:01 +0200

    Create DepartmentsRepository.php

---
+++ modules/Structure/Repository/DepartmentsRepository.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+namespace App\Modules\Structure\Repository;
+
+use App\Modules\Structure\Entity\Departments;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Departments>
+ */
+class DepartmentsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Departments::class);
+    }
+
+    /**
+     * @return Departments[] Returns an array of Departments objects
+     *
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findByExampleField($value): array
+    {
+        /* @phpstan-ignore doctrine.dql */
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('d.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findOneBySomeField($value): ?Departments
+    {
+        /* @phpstan-ignore doctrine.dql */
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
