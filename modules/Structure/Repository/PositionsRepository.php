de154193a7b24325c55b61a1c32a9ebe42a68084
Ярослав <yaroslaw74@gmail.com>
2026-03-25 20:35:08 +0200

    Create PositionsRepository.php

---
+++ modules/Structure/Repository/PositionsRepository.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+namespace App\Modules\Structure\Repository;
+
+use App\Modules\Structure\Entity\Positions;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Positions>
+ */
+class PositionsRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Positions::class);
+    }
+
+    /**
+     * @return Positions[] Returns an array of Positions objects
+     *
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findByExampleField($value): array
+    {
+        /* @phpstan-ignore doctrine.dql */
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findOneBySomeField($value): ?Positions
+    {
+        /* @phpstan-ignore doctrine.dql */
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
