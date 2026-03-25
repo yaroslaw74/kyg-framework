b0b2e20955cb62d353d3d531ce1813aaf0374d9c
Ярослав <yaroslaw74@gmail.com>
2026-03-25 20:34:57 +0200

    Create Positions.php

---
+++ modules/Structure/Entity/Positions.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+namespace App\Modules\Structure\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Modules\Employees\Entity\Employees;
+use App\Modules\Structure\Repository\PositionsRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Doctrine\DBAL\Types\Types;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: PositionsRepository::class)]
+#[ORM\Table(name: 'structure__positions')]
+#[Gedmo\SoftDeleteable]
+#[ApiResource]
+class Positions
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(type: Types::STRING, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'positions')]
+    private ?Departments $department = null;
+
+    #[ORM\ManyToOne(inversedBy: 'positions')]
+    private ?Employees $employee = null;
+
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
+
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+
+        return $data;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __unserialize(array $data): void
+    {
+        [
+            $this->id,
+            $this->name,
+            $this->createdAt,
+            $this->createdBy,
+            $this->updatedAt,
+            $this->updatedBy,
+            $this->deletedAt,
+            $this->department,
+            $this->employee,
+        ] = $data;
+    }
+
+    public function getId(): ?string
+    {
+        if (null !== $this->id) {
+            return $this->id->toString();
+        }
+
+        return null;
+    }
+
+    public function getUuid(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDepartment(): ?Departments
+    {
+        return $this->department;
+    }
+
+    public function setDepartment(?Departments $department): static
+    {
+        $this->department = $department;
+
+        return $this;
+    }
+
+    public function getEmployee(): ?Employees
+    {
+        return $this->employee;
+    }
+
+    public function setEmployee(?Employees $employee): static
+    {
+        $this->employee = $employee;
+
+        return $this;
+    }
+}
