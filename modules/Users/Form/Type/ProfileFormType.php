c4e5ff29241b99872a07a2fc7a21f2bdfa9a550a
Ярослав <yaroslaw74@gmail.com>
2026-03-28 21:41:37 +0200

    Update ProfileFormType.php

--- modules/Users/Form/Type/ProfileFormType.php
+++ modules/Users/Form/Type/ProfileFormType.php
@@ -17,6 +17,8 @@ namespace App\Modules\Users\Form\Type;
 use App\Modules\Users\Entity\User;
 use App\Settings\NameSettings;
 use Jbtronics\SettingsBundle\Manager\SettingsManagerInterface;
+use libphonenumber\PhoneNumberFormat;
+use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -79,8 +81,12 @@ class ProfileFormType extends AbstractType
                     'placeholder' => $this->translator->trans('Enter Email', [], 'users'),
                 ],
             ])
-            ->add('mobile', TextType::class, [
+            ->add('mobile', PhoneNumberType::class, [
                 'label' => $this->translator->trans('Phone', [], 'users'),
+                'format' => PhoneNumberFormat::INTERNATIONAL,
+                'number_type' => PhoneNumberType::NUMBER_TYPE_TEL,
+                'widget' => PhoneNumberType::WIDGET_COUNTRY_CHOICE,
+                'country_display_type' => 'display_country_full',
                 'required' => false,
                 'row_attr' => [
                     'class' => 'form-group mb-3',
