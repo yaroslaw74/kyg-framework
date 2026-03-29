3203e97dec72cbc06a571718709e77e05f082fae
Ярослав <yaroslaw74@gmail.com>
2026-03-29 20:11:59 +0300

    Create MessageMetadata.php

---
+++ modules/Chat/Entity/MessageMetadata.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Chat\Entity;
+
+use App\Modules\Chat\Repository\MessageMetadataRepository;
+use App\Modules\Users\Entity\User;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: MessageMetadataRepository::class)]
+#[ORM\Table(name: 'chat_message_metadata')]
+#[Gedmo\SoftDeleteable]
+class MessageMetadata
+{
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+    use TimestampableEntity;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BOOLEAN, options: ['default' => false])]
+    protected bool $isRead = false;
+
+    #[ORM\ManyToOne(inversedBy: 'metadata')]
+    private ?Message $message = null;
+
+    #[ORM\ManyToOne]
+    private ?User $participant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsRead(): bool
+    {
+        return $this->isRead;
+    }
+
+    public function setIsRead(bool $isRead): static
+    {
+        $this->isRead = $isRead;
+
+        return $this;
+    }
+
+    public function getMessage(): ?Message
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?Message $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getParticipant(): ?User
+    {
+        return $this->participant;
+    }
+
+    public function setParticipant(?User $participant): static
+    {
+        $this->participant = $participant;
+
+        return $this;
+    }
+}
