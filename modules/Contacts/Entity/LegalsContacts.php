389714930f785f480e0d260f127cbbd9dec82d91
Ярослав <yaroslaw74@gmail.com>
2026-04-02 21:46:02 +0300

    Update LegalsContacts.php

--- modules/Contacts/Entity/LegalsContacts.php
+++ modules/Contacts/Entity/LegalsContacts.php
@@ -28,7 +28,9 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
 #[Gedmo\SoftDeleteable]
 class LegalsContacts
 {
-    use SoftDeleteableEntity, BlameableEntity, TimestampableEntity;
+    use SoftDeleteableEntity;
+    use BlameableEntity;
+    use TimestampableEntity;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
