10a9457cc8ba7f4be2178baa7a4c070e9a462fed
Ярослав <yaroslaw74@gmail.com>
2026-04-02 21:48:36 +0300

    Create CodeRepository.php

---
+++ src/Repository/CodeRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Repository
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Code;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Code>
+ */
+class CodeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Code::class);
+    }
+
+    /**
+     * @return Code[] Returns an array of Code objects
+     *
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findByExampleField($value): array
+    {
+        /*
+         * @phpstan-ignore doctrine.dql
+         */
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @phpstan-ignore missingType.parameter
+     */
+    public function findOneBySomeField($value): ?Code
+    {
+        /*
+         * @phpstan-ignore doctrine.dql
+         */
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
