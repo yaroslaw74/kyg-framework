36c39ee8e7b507b9afd81173e16c7fc2c6ca61d1
Ярослав <yaroslaw74@gmail.com>
2026-04-05 14:28:11 +0300

    Update ThreadMetadata.php

--- modules/Chat/Entity/ThreadMetadata.php
+++ modules/Chat/Entity/ThreadMetadata.php
@@ -18,16 +18,11 @@ use App\Modules\Chat\Repository\ThreadMetadataRepository;
 use App\Modules\Users\Entity\User;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Blameable\Traits\BlameableEntity;
-use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: ThreadMetadataRepository::class)]
 #[ORM\Table(name: 'chat__thread_metadata')]
 class ThreadMetadata
 {
-    use BlameableEntity;
-    use TimestampableEntity;
-
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: Types::INTEGER)]
