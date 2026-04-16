baaca1ad2a2cf3f56aff00c029f4c52aea0e0b35
Ярослав <yaroslaw74@gmail.com>
2026-04-16 22:12:43 +0300

    Update Permission.php

--- modules/Roles/Entity/Permission.php
+++ modules/Roles/Entity/Permission.php
@@ -17,6 +17,7 @@ namespace App\Modules\Roles\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use PhpRbacBundle\Entity\Permission as EntityPermission;
 use PhpRbacBundle\Repository\PermissionRepository;
+use Gedmo\Blameable\Traits\BlameableEntity;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
