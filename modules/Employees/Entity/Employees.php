a10f810011222be1d8d9a656903af489f2681abf
Ярослав <yaroslaw74@gmail.com>
2026-04-26 15:22:46 +0300

    Update Employees.php

--- modules/Employees/Entity/Employees.php
+++ modules/Employees/Entity/Employees.php
@@ -15,13 +15,11 @@ declare(strict_types=1);
 namespace App\Modules\Employees\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use App\Interfaces\CodeInterface;
 use App\Modules\Contacts\Entity\EmployeesContacts;
 use App\Modules\Employees\Repository\EmployeesRepository;
 use App\Modules\Persons\Entity\Natural;
 use App\Modules\Structure\Entity\Positions;
 use App\Modules\Users\Entity\User;
-use App\Traits\CodeTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
@@ -39,12 +37,11 @@ use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 #[Gedmo\Uploadable(pathMethod: 'path', filenameGenerator: Validator::FILENAME_GENERATOR_SHA1, allowOverwrite: true, appendNumber: true)]
 #[Gedmo\SoftDeleteable]
 #[ApiResource]
-class Employees implements CodeInterface
+class Employees
 {
     use SoftDeleteableEntity;
     use BlameableEntity;
     use TimestampableEntity;
-    use CodeTrait;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -92,7 +89,6 @@ class Employees implements CodeInterface
     {
         $this->contacts = new ArrayCollection();
         $this->positions = new ArrayCollection();
-        $this->addCode();
     }
 
     public function __serialize(): array
@@ -124,7 +120,6 @@ class Employees implements CodeInterface
             $this->user,
             $this->contacts,
             $this->positions,
-            $this->code,
         ] = $data;
     }
 
