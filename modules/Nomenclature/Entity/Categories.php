92ab1043813a03574dbe230861e270b857942bca
Ярослав <yaroslaw74@gmail.com>
2026-04-26 15:22:50 +0300

    Update Categories.php

--- modules/Nomenclature/Entity/Categories.php
+++ modules/Nomenclature/Entity/Categories.php
@@ -15,9 +15,7 @@ declare(strict_types=1);
 namespace App\Modules\Nomenclature\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use App\Interfaces\CodeInterface;
 use App\Modules\Nomenclature\Entity\Translation\CategoriesTranslation;
-use App\Traits\CodeTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
@@ -35,12 +33,11 @@ use Gedmo\Translatable\Translatable;
 #[Gedmo\SoftDeleteable]
 #[Gedmo\TranslationEntity(class: CategoriesTranslation::class)]
 #[ApiResource]
-class Categories implements Sortable, Translatable, CodeInterface
+class Categories implements Sortable, Translatable
 {
     use SoftDeleteableEntity;
     use BlameableEntity;
     use TimestampableEntity;
-    use CodeTrait;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -92,7 +89,6 @@ class Categories implements Sortable, Translatable, CodeInterface
     {
         $this->translations = new ArrayCollection();
         $this->nomenclatures = new ArrayCollection();
-        $this->addCode();
     }
 
     public function __toString(): string
@@ -128,7 +124,6 @@ class Categories implements Sortable, Translatable, CodeInterface
             $this->locale,
             $this->translations,
             $this->nomenclatures,
-            $this->code,
         ] = $data;
     }
 
