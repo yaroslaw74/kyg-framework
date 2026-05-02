64f910e23628ffc4ae3972172838415ba7b7ed90
Ярослав <yaroslaw74@gmail.com>
2026-05-02 20:33:58 +0300

    Update NaturalsContacts.php

--- modules/Contacts/Entity/NaturalsContacts.php
+++ modules/Contacts/Entity/NaturalsContacts.php
@@ -28,8 +28,8 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
 #[Gedmo\SoftDeleteable]
 class NaturalsContacts
 {
-    use SoftDeleteableEntity;
     use BlameableEntity;
+    use SoftDeleteableEntity;
     use TimestampableEntity;
 
     #[ORM\Id]
@@ -59,12 +59,12 @@ class NaturalsContacts
     public function __unserialize(array $data): void
     {
         [
-            $this->id,
             $this->createdAt,
             $this->createdBy,
             $this->updatedAt,
             $this->updatedBy,
             $this->deletedAt,
+            $this->id,
             $this->individual,
             $this->name,
             $this->value,
