a8730ab8317652ae5848785c3848b3bde4898590
Ярослав <yaroslaw74@gmail.com>
2026-05-03 19:35:41 +0300

    Update RefreshToken.php

--- src/Entity/RefreshToken.php
+++ src/Entity/RefreshToken.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Entity
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
