2f93935e3479cfdd54e4dcd792521b68ae27c2b9
Ярослав <yaroslaw74@gmail.com>
2026-05-24 20:24:34 +0300

    Update ecs.php

--- ecs.php
+++ ecs.php
@@ -7,7 +7,7 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $config): void {
     $config->import('vendor/sylius-labs/coding-standard/ecs.php');
-    $config->paths(['src', 'test', 'modules', 'public/additions']);
+    $config->paths(['src', 'tests', 'modules', 'public/additions', 'config']);
 
     $config->skip([
         VisibilityRequiredFixer::class => ['*Spec.php'],
