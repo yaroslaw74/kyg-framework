0c1c88b32367051fa1af14ff0985ff829d9a3e5f
Ярослав <yaroslaw74@gmail.com>
2026-05-24 16:04:14 +0300

    Update console-application.php

--- tests/console-application.php
+++ tests/console-application.php
@@ -14,10 +14,10 @@ use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $dotenv = new Dotenv();
-$dotenv->bootEnv(__DIR__ . '/../.env');
+$dotenv->bootEnv(__DIR__.'/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 
