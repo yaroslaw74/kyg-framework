4f00efc411c21b6ab63c1ac71461e488af88bb36
Ярослав <yaroslaw74@gmail.com>
2026-05-24 20:35:07 +0300

    Delete menuClose.js

--- assets/js/functions/menuClose.js
+++
@@ -1,6 +0,0 @@
-export function menuClose() {
-    const HTML = document.querySelector("html");
-    HTML.setAttribute("data-toggled", "close");
-    const RESPONSIVE_OVERLAY = document.querySelector("#responsive-overlay");
-    RESPONSIVE_OVERLAY.classList.remove("active");
-}
