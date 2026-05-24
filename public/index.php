34570adba87c532a76654be5d3dea181a359adfb
Ярослав <yaroslaw74@gmail.com>
2026-05-24 15:18:26 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@
  * @version    1.0.0
  *
  * @copyright  Copyright (c) Kataev Yaroslav
- * @license    GNU General Public License version 3 or later, see LICENSE
+ * @license    The MIT License, see LICENSE
  */
 declare(strict_types=1);
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context): Kernel {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
