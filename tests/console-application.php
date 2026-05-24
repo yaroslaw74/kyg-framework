7ad1f8389a13614b4d7e2ed7193b8ab6533aee4d
Ярослав <yaroslaw74@gmail.com>
2026-05-24 15:32:34 +0300

    Update console-application.php

--- tests/console-application.php
+++ tests/console-application.php
@@ -8,16 +8,16 @@
  * @version    1.0.0
  *
  * @copyright  Copyright (c) Kataev Yaroslav
- * @license    GNU General Public License version 3 or later, see LICENSE
+ * @license    The MIT License, see LICENSE
  */
 use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = new Dotenv();
-$dotenv->bootEnv(__DIR__.'/../.env');
+$dotenv->bootEnv(__DIR__ . '/../.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 
