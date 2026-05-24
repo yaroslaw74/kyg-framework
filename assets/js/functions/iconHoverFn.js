07049c691cb5bbd2b2e71e37c723e133d6ca1efe
Ярослав <yaroslaw74@gmail.com>
2026-05-24 20:34:42 +0300

    Delete iconHoverFn.js

--- assets/js/functions/iconHoverFn.js
+++
@@ -1,9 +0,0 @@
-import { clearNavDropdown } from "./clearNavDropdown.js";
-import { toggleSidemenu } from "./toggleSidemenu.js";
-
-export function iconHoverFn() {
-    const HTML = document.querySelector("html");
-    HTML.setAttribute("data-nav-style", "icon-hover");
-    toggleSidemenu();
-    clearNavDropdown();
-}
