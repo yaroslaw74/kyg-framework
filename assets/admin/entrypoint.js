08790086e32b6cf704820a298e24f549d2e153a0
Ярослав <yaroslaw74@gmail.com>
2026-05-24 21:12:34 +0300

    delite

--- assets/admin/entrypoint.js
+++
@@ -1,8 +0,0 @@
-import "@vendor/sylius/mollie-plugin/assets/admin/entrypoint";
-import "@vendor/sylius/cms-plugin/assets/admin/entrypoint";
-import "@vendor/sylius/refund-plugin/assets/entrypoint";
-import "./stimulus_bootstrap.js";
-import "../stimulus_bootstrap.js";
-
-// In this file you can import assets like images or stylesheets
-console.log("Hello Webpack Encore! Edit me in assets/admin/entrypoint.js");
--- assets/shop/entrypoint.js
+++
@@ -1,7 +0,0 @@
-import "@vendor/sylius/mollie-plugin/assets/shop/entrypoint";
-import "@vendor/sylius/cms-plugin/assets/shop/entrypoint";
-import "./stimulus_bootstrap.js";
-import "../stimulus_bootstrap.js";
-
-// In this file you can import assets like images or stylesheets
-console.log("Hello Webpack Encore! Edit me in assets/shop/entrypoint.js");
