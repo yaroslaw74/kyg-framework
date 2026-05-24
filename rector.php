f912b6dd84669d7efeadaa16d981e9845bb6716b
Ярослав <yaroslaw74@gmail.com>
2026-05-24 16:03:15 +0300

    Update rector.php

--- rector.php
+++ rector.php
@@ -2,17 +2,15 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
-    $rectorConfig->import(__DIR__ . '/vendor/sylius/sylius-rector/config/config.php');
+    $rectorConfig->import(__DIR__.'/vendor/sylius/sylius-rector/config/config.php');
     $rectorConfig->paths([
-        __DIR__ . '/modules',
-        __DIR__ . '/src',
-        __DIR__ . '/test',
-        __DIR__ . '/public/additions',
+        __DIR__.'/modules',
+        __DIR__.'/src',
+        __DIR__.'/test',
+        __DIR__.'/public/additions',
     ]);
 };
