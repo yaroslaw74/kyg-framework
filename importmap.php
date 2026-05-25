3ad50a90db5f5465e81493b38fa07d68159f3f6c
Ярослав <yaroslaw74@gmail.com>
2026-05-25 18:49:01 +0300

    Update importmap.php

--- importmap.php
+++ importmap.php
@@ -16,13 +16,588 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'ltr' => [
+        'path' => './assets/ltr.js',
+        'entrypoint' => true,
+    ],
+    'rtl' => [
+        'path' => './assets/rtl.js',
+        'entrypoint' => true,
+    ],
+    '@rekalogika/file-filepond' => [
+        'path' => './vendor/rekalogika/file-filepond/assets/dist/filepond.js',
+    ],
+    '@rekalogika/temporary-url-bundle' => [
+        'path' => './vendor/rekalogika/temporary-url-bundle/assets/dist/autoexpire.js',
+    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@symfony/ux-autocomplete' => [
+        'path' => './vendor/symfony/ux-autocomplete/assets/dist/controller.js',
+    ],
+    '@symfony/ux-chartjs' => [
+        'path' => './vendor/symfony/ux-chartjs/assets/dist/controller.js',
+    ],
+    '@symfony/ux-cropperjs' => [
+        'path' => './vendor/symfony/ux-cropperjs/assets/dist/controller.js',
+    ],
+    '@symfony/ux-dropzone' => [
+        'path' => './vendor/symfony/ux-dropzone/assets/dist/controller.js',
+    ],
+    '@symfony/ux-google-map' => [
+        'path' => './vendor/symfony/ux-google-map/assets/dist/map_controller.js',
+    ],
+    '@symfony/ux-live-component' => [
+        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
+    '@symfony/ux-map' => [
+        'path' => './vendor/symfony/ux-map/assets/dist/abstract_map_controller.js',
+    ],
+    '@symfony/ux-turbo' => [
+        'path' => './vendor/symfony/ux-turbo/assets/dist/turbo_controller.d.ts',
     ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
     '@hotwired/turbo' => [
         'version' => '8.0.23',
+    ],
+    '@googlemaps/js-api-loader' => [
+        'version' => '2.0.2',
+    ],
+    '@symfony/ux-toggle-password' => [
+        'version' => '2.35.0',
+    ],
+    '@symfony/ux-toggle-password/dist/style.min.css' => [
+        'version' => '2.35.0',
+        'type' => 'css',
+    ],
+    '@symfony/ux-swup' => [
+        'version' => '2.35.0',
+    ],
+    '@swup/debug-plugin' => [
+        'version' => '4.1.0',
+    ],
+    '@swup/theme' => [
+        'version' => '2.1.0',
+    ],
+    '@swup/fade-theme' => [
+        'version' => '2.0.2',
+    ],
+    '@swup/forms-plugin' => [
+        'version' => '3.6.0',
+    ],
+    '@swup/slide-theme' => [
+        'version' => '2.0.2',
+    ],
+    'swup' => [
+        'version' => '4.9.0',
+    ],
+    '@swup/plugin' => [
+        'version' => '4.0.0',
+    ],
+    'delegate-it' => [
+        'version' => '6.4.0',
+    ],
+    'path-to-regexp' => [
+        'version' => '8.4.2',
+    ],
+    'es-module-shims' => [
+        'version' => '2.8.1',
+    ],
+    'chart.js' => [
+        'version' => '4.5.1',
+    ],
+    'chart.js/helpers' => [
+        'version' => '4.5.1',
+    ],
+    'chartjs-plugin-annotation' => [
+        'version' => '3.1.0',
+    ],
+    'chartjs-plugin-datalabels' => [
+        'version' => '2.2.0',
+    ],
+    'chartjs-plugin-zoom' => [
+        'version' => '2.2.0',
+    ],
+    '@kurkle/color' => [
+        'version' => '0.4.0',
+    ],
+    'hammerjs' => [
+        'version' => '2.0.8',
+    ],
+    'tom-select' => [
+        'version' => '2.6.1',
+    ],
+    'tom-select/dist/css/tom-select.bootstrap5.css' => [
+        'version' => '2.6.1',
+        'type' => 'css',
+    ],
+    'tom-select/dist/css/tom-select.bootstrap4.css' => [
+        'version' => '2.6.1',
+        'type' => 'css',
+    ],
+    'tom-select/dist/css/tom-select.default.css' => [
+        'version' => '2.6.1',
+        'type' => 'css',
+    ],
+    'tom-select/dist/css/tom-select.default.min.css' => [
+        'version' => '2.6.1',
+        'type' => 'css',
+    ],
+    '@orchidjs/sifter' => [
+        'version' => '1.1.0',
+    ],
+    '@orchidjs/unicode-variants' => [
+        'version' => '1.1.2',
+    ],
+    'bootstrap-icons/font/bootstrap-icons.min.css' => [
+        'version' => '1.13.1',
+        'type' => 'css',
+    ],
+    'boxicons/css/boxicons.min.css' => [
+        'version' => '2.1.4',
+        'type' => 'css',
+    ],
+    '@fortawesome/fontawesome-free/css/all.min.css' => [
+        'version' => '7.2.0',
+        'type' => 'css',
+    ],
+    'line-awesome/dist/line-awesome/css/line-awesome.min.css' => [
+        'version' => '1.3.0',
+        'type' => 'css',
+    ],
+    '@mdi/font/css/materialdesignicons.min.css' => [
+        'version' => '7.4.47',
+        'type' => 'css',
+    ],
+    'pixeden-stroke-7-icon/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css' => [
+        'version' => '1.2.3',
+        'type' => 'css',
+    ],
+    'remixicon/fonts/remixicon.css' => [
+        'version' => '4.9.1',
+        'type' => 'css',
+    ],
+    'simple-line-icons/css/simple-line-icons.css' => [
+        'version' => '2.5.5',
+        'type' => 'css',
+    ],
+    '@tabler/icons-webfont/dist/tabler-icons.min.css' => [
+        'version' => '3.44.0',
+        'type' => 'css',
+    ],
+    '@icon/themify-icons/themify-icons.css' => [
+        'version' => '1.0.1-alpha.3',
+        'type' => 'css',
+    ],
+    'typicons.font/src/font/typicons.css' => [
+        'version' => '2.1.2',
+        'type' => 'css',
+    ],
+    'weather-icons/css/weather-icons.min.css' => [
+        'version' => '1.3.2',
+        'type' => 'css',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
+    'bootstrap/dist/css/bootstrap.rtl.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'node-waves' => [
+        'version' => '0.7.6',
+    ],
+    'node-waves/dist/waves.min.css' => [
+        'version' => '0.7.6',
+        'type' => 'css',
+    ],
+    'simplebar' => [
+        'version' => '6.3.3',
+    ],
+    'simplebar/dist/simplebar.min.css' => [
+        'version' => '6.3.3',
+        'type' => 'css',
+    ],
+    'simplebar-core' => [
+        'version' => '1.3.2',
+    ],
+    'simplebar-core/dist/simplebar.min.css' => [
+        'version' => '1.3.2',
+        'type' => 'css',
+    ],
+    'lodash-es/debounce.js' => [
+        'version' => '4.18.1',
+    ],
+    'lodash-es/throttle.js' => [
+        'version' => '4.18.1',
+    ],
+    'resize-observer-polyfill' => [
+        'version' => '1.5.1',
+    ],
+    '@simonwep/pickr/dist/pickr.es5.min.js' => [
+        'version' => '1.9.1',
+    ],
+    '@simonwep/pickr/dist/themes/nano.min.css' => [
+        'version' => '1.9.1',
+        'type' => 'css',
+    ],
+    'stimulus-flatpickr' => [
+        'version' => '1.4.0',
+    ],
+    'stimulus' => [
+        'version' => '3.2.2',
+    ],
+    'flatpickr' => [
+        'version' => '4.6.13',
+    ],
+    'flatpickr/dist/flatpickr.min.css' => [
+        'version' => '4.6.13',
+        'type' => 'css',
+    ],
+    '@stimulus/core' => [
+        'version' => '2.0.0',
+    ],
+    '@stimulus/mutation-observers' => [
+        'version' => '2.0.0',
+    ],
+    '@stimulus/multimap' => [
+        'version' => '2.0.0',
+    ],
+    'choices.js' => [
+        'version' => '11.2.3',
+    ],
+    'choices.js/public/assets/styles/choices.min.css' => [
+        'version' => '11.2.3',
+        'type' => 'css',
+    ],
+    'filepond' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/dist/filepond.min.css' => [
+        'version' => '4.32.12',
+        'type' => 'css',
+    ],
+    'filepond/locale/ar-ar.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/cs-cz.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/da-dk.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/de-de.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/el-el.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/en-en.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/es-es.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/fa_ir.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/fi-fi.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/fr-fr.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/he-he.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/hr-hr.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/hu-hu.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/id-id.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/it-it.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/ja-ja.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/lt-lt.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/nl-nl.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/no_nb.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/pl-pl.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/pt-br.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/ro-ro.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/ru-ru.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/sk-sk.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/sv_se.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/tr-tr.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/uk-ua.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/vi-vi.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/zh-cn.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond/locale/zh-tw.js' => [
+        'version' => '4.32.12',
+    ],
+    'filepond-plugin-file-encode' => [
+        'version' => '2.1.14',
+    ],
+    'filepond-plugin-file-metadata' => [
+        'version' => '1.0.8',
+    ],
+    'filepond-plugin-file-poster' => [
+        'version' => '2.5.2',
+    ],
+    'filepond-plugin-file-poster/dist/filepond-plugin-file-poster.css' => [
+        'version' => '2.5.2',
+        'type' => 'css',
+    ],
+    'filepond-plugin-file-validate-size' => [
+        'version' => '2.2.8',
+    ],
+    'filepond-plugin-file-validate-type' => [
+        'version' => '1.2.9',
+    ],
+    'filepond-plugin-image-crop' => [
+        'version' => '2.0.6',
+    ],
+    'filepond-plugin-image-edit' => [
+        'version' => '1.6.3',
+    ],
+    'filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css' => [
+        'version' => '1.6.3',
+        'type' => 'css',
+    ],
+    'filepond-plugin-image-exif-orientation' => [
+        'version' => '1.0.11',
+    ],
+    'filepond-plugin-image-preview' => [
+        'version' => '4.6.12',
+    ],
+    'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css' => [
+        'version' => '4.6.12',
+        'type' => 'css',
+    ],
+    'filepond-plugin-image-resize' => [
+        'version' => '2.0.10',
+    ],
+    'filepond-plugin-image-transform' => [
+        'version' => '3.8.7',
+    ],
+    'filepond-plugin-image-validate-size' => [
+        'version' => '1.2.7',
+    ],
+    'apexcharts' => [
+        'version' => '5.13.0',
+    ],
+    'apexcharts/dist/apexcharts.css' => [
+        'version' => '5.13.0',
+        'type' => 'css',
+    ],
+    'echarts' => [
+        'version' => '6.1.0',
+    ],
+    'tslib' => [
+        'version' => '2.8.1',
+    ],
+    'zrender/lib/zrender.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/util.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/env.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/timsort.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/Eventful.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/Text.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/tool/color.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/Path.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/tool/path.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/matrix.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/vector.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/Transformable.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/Image.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/Group.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Circle.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Ellipse.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Sector.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Ring.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Polygon.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Polyline.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Rect.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Line.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/BezierCurve.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/shape/Arc.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/CompoundPath.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/LinearGradient.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/RadialGradient.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/BoundingRect.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/OrientedBoundingRect.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/Point.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/IncrementalDisplayable.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/helper/subPixelOptimize.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/dom.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/helper/parseText.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/WeakMap.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/LRU.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/contain/text.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/canvas/graphic.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/platform.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/contain/polygon.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/PathProxy.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/contain/util.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/curve.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/svg/Painter.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/canvas/Painter.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/event.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/tool/parseSVG.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/tool/parseXML.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/graphic/Displayable.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/core/bbox.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/contain/line.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/contain/quadratic.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/animation/Animator.js' => [
+        'version' => '6.1.0',
+    ],
+    'zrender/lib/tool/morphPath.js' => [
+        'version' => '6.1.0',
     ],
 ];
