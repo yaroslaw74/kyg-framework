6b0a184fe523b0bf38290575b46210fc64f69dec
Ярослав <yaroslaw74@gmail.com>
2026-05-25 21:15:59 +0300

    Create menuhoverFn.js

---
+++ assets/scripts/functions/menuhoverFn.js
@@ -0,0 +1,13 @@
+import { clearNavDropdown } from "./clearNavDropdown.js";
+import { toggleSidemenu } from "./toggleSidemenu.js";
+
+export function menuhoverFn() {
+    const HTML = document.querySelector("html");
+    HTML.setAttribute("data-nav-style", "menu-hover");
+    HTML.removeAttribute("data-hor-style");
+    HTML.removeAttribute("data-vertical-style");
+    toggleSidemenu();
+    if (HTML.getAttribute("data-toggled") === "menu-hover-closed") {
+        clearNavDropdown();
+    }
+}
