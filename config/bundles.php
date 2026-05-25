c53188647a7d430134657885ee0c7f1a1efc9453
Ярослав <yaroslaw74@gmail.com>
2026-05-25 18:53:38 +0300

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -35,4 +35,5 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
