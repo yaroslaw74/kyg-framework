96d3eb15a78b8201e846066666369d9433b20fd2
Ярослав <yaroslaw74@gmail.com>
2026-05-25 21:14:37 +0300

    Create count-up.js

---
+++ assets/scripts/count-up.js
@@ -0,0 +1,11 @@
+(() => {
+    let i = 1;
+    setInterval(() => {
+        document.querySelectorAll(".count-up").forEach((ele) => {
+            if (ele.getAttribute("data-count") >= i) {
+                i = i + 1;
+                ele.innerText = i;
+            }
+        });
+    }, 10);
+})();
