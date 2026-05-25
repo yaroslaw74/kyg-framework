b20c65a98ce053bf8a169418d89863fdcc6f1350
Ярослав <yaroslaw74@gmail.com>
2026-05-25 21:16:58 +0300

    Create valex.js

---
+++ assets/valex.js
@@ -0,0 +1,38 @@
+/*
+ * Valex – Premium Bootstrap-5 Admin Template
+ */
+
+/*
+ * Styles
+ */
+//import "./css/styles.css";
+
+/*
+ * Widgets
+ */
+
+/*
+ * Modules
+ */
+
+/* Custom JS */
+import "./scripts/custom.js";
+
+/* Custom-Switcher JS */
+import "./scripts/custom-switcher.js";
+
+/* Defaultmenu JS */
+import "./scripts/defaultmenu.js";
+
+/* Main Theme Js */
+import "./scripts/main.js";
+import "./scripts/authentication-main.js";
+
+/* Page loader JS */
+import "./scripts/page-loader.js";
+
+/* Simplebar JS */
+import "./scripts/simplebar.js";
+
+/* Sticky JS */
+import "./scripts/sticky.js";
