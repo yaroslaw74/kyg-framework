45a69d2e71a59935c91008a403f16af807dbbd4f
Ярослав <yaroslaw74@gmail.com>
2026-05-28 20:49:55 +0300

    Create AbstractCharts.php

---
+++ modules/Charts/Extension/AbstractCharts.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category Charts Extension Abstract Class
+ *
+ * @version 1.0.0
+ *
+ * @copyright Copyright (c) Kataev Yaroslav
+ * @license GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Charts\Extension;
+
+abstract class AbstractCharts implements ChartsInterface
+{
+    protected string $script;
+
+    protected string $name;
+
+    /**
+     * @var array<string, mixed>
+     */
+    protected array $options;
+
+    public function getScript(): string
+    {
+        return $this->script;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+}
