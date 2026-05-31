1a1390ed60d18f80343a54d9580c37ae9df43747
Ярослав <yaroslaw74@gmail.com>
2026-05-31 14:50:25 +0300

    Create AppTwigExtension.php

---
+++ src/Twig/AppTwigExtension.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Twig Extension
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Service\LocalesService;
+use App\Settings\TwigGlobalsSettings;
+use Jbtronics\SettingsBundle\Manager\SettingsManagerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\ExtensionInterface;
+use Twig\Extension\GlobalsInterface;
+use Twig\TwigFunction;
+
+class AppTwigExtension extends AbstractExtension implements ExtensionInterface, GlobalsInterface
+{
+    public function __construct(
+        private LocalesService $localesService,
+        private SettingsManagerInterface $settingsManager,
+    ) {
+    }
+
+    public function LocaleDirExtension(string $locale): string
+    {
+        return $this->localesService->getLocaleDir($locale);
+    }
+
+    public function LocaleHTMLExtension(string $locale): string
+    {
+        return $this->localesService->getLocaleHTML($locale);
+    }
+
+    public function LocaleIsFullExtension(string $locale): bool
+    {
+        return $this->localesService->isFull($locale);
+    }
+
+    /**
+     * @return TwigFunction[]
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('locale_dir', [$this, 'LocaleDirExtension']),
+            new TwigFunction('locale_HTML', [$this, 'LocaleHTMLExtension']),
+            new TwigFunction('locale_Full', [$this, 'LocaleIsFullExtension']),
+        ];
+    }
+
+    /**
+     * @return array<string, string|bool>
+     */
+    public function getGlobals(): array
+    {
+        $settings = $this->settingsManager->get(TwigGlobalsSettings::class);
+
+        return [
+            'app_name' => $settings->getName(),
+            'app_desktop_logo' => $settings->getDesktopLogo(),
+            'app_toggle_logo' => $settings->getToggleLogo(),
+            'app_desktop_white' => $settings->getDesktopWhite(),
+            'app_toggle_white' => $settings->getToggleWhite(),
+        ];
+    }
+}
