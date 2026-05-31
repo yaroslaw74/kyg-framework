09816d3a470cc3089e3888d97f7e8f06b5f69a17
Ярослав <yaroslaw74@gmail.com>
2026-05-31 13:33:39 +0300

    Create CoreController.php

---
+++ src/Controller/CoreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CoreController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('core/index.html.twig', [
+            'controller_name' => 'CoreController',
+        ]);
+    }
+}
