52c3c3916dd8e3c4f6736ba71213aee9f1501650
Ярослав <yaroslaw74@gmail.com>
2026-05-31 13:54:57 +0300

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Mailer\Test\ZenstruckMailerTestBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
+    Nelmio\JsLoggerBundle\NelmioJsLoggerBundle::class => ['all' => true],
 ];
