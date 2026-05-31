d96b4b3c3ef4c708f0e37ced1a3d5885356f08b1
Ярослав <yaroslaw74@gmail.com>
2026-05-31 21:36:35 +0300

    Update reference.php

--- config/reference.php
+++ config/reference.php
@@ -1651,6 +1651,25 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     },
  *     ignore_not_found?: bool|Param, // Ignore error when an icon is not found. Set to 'true' to fail silently. // Default: false
  * }
+ * @psalm-type SymfonycastsSassConfig = array{
+ *     root_sass?: list<scalar|Param|null>,
+ *     binary?: scalar|Param|null, // The Sass binary to use // Default: null
+ *     search_for_binary?: scalar|Param|null, // Whether to search for the Sass binary in the system PATH // Default: true
+ *     sass_options?: array{
+ *         style?: "compressed"|"expanded"|Param, // The style of the generated CSS: compressed or expanded. // Default: "expanded"
+ *         charset?: bool|Param, // Whether to include the charset declaration in the generated Sass.
+ *         error_css?: bool|Param, // Emit a CSS file when an error occurs.
+ *         source_map?: bool|Param, // Whether to generate source maps. // Default: true
+ *         embed_sources?: bool|Param, // Embed source file contents in source maps.
+ *         embed_source_map?: bool|Param, // Embed source map contents in CSS. // Default: "%kernel.debug%"
+ *         load_path?: list<scalar|Param|null>,
+ *         quiet?: bool|Param, // Don't print warnings.
+ *         quiet_deps?: bool|Param, // Don't print compiler warnings from dependencies.
+ *         stop_on_error?: bool|Param, // Don't compile more files once an error is encountered.
+ *         trace?: bool|Param, // Print full Dart stack traces for exceptions.
+ *     },
+ *     embed_sourcemap?: bool|Param|null, // Deprecated: Option "embed_sourcemap" at "symfonycasts_sass.embed_sourcemap" is deprecated. Use "sass_options.embed_source_map" instead". // Default: null
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1671,6 +1690,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     nelmio_js_logger?: NelmioJsLoggerConfig,
  *     jbtronics_settings?: JbtronicsSettingsConfig,
  *     ux_icons?: UxIconsConfig,
+ *     symfonycasts_sass?: SymfonycastsSassConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1695,6 +1715,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         nelmio_js_logger?: NelmioJsLoggerConfig,
  *         jbtronics_settings?: JbtronicsSettingsConfig,
  *         ux_icons?: UxIconsConfig,
+ *         symfonycasts_sass?: SymfonycastsSassConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1717,6 +1738,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         nelmio_js_logger?: NelmioJsLoggerConfig,
  *         jbtronics_settings?: JbtronicsSettingsConfig,
  *         ux_icons?: UxIconsConfig,
+ *         symfonycasts_sass?: SymfonycastsSassConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
