bc442c212deb57acd1e3544a5063f94dbadccf88
Ярослав <yaroslaw74@gmail.com>
2026-06-08 20:43:55 +0300

    Create LocaleSubscriber.php

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Event Subscriber
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private string $defaultLocale;
+
+    public function __construct(private ContainerBagInterface $params)
+    {
+        $this->defaultLocale = $this->params->get('kernel.default_locale');
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        $locale = $request->request->getString('_locale');
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ('' !== $locale) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
