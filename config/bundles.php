e02a235dc52f4a6fe68aeb0400955dc9f0146ef9
Ярослав <yaroslaw74@gmail.com>
2026-06-14 16:24:13 +0300

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -34,4 +34,5 @@ return [
     Nelmio\JsLoggerBundle\NelmioJsLoggerBundle::class => ['all' => true],
     Shivas\VersioningBundle\ShivasVersioningBundle::class => ['all' => true],
     Jbtronics\SettingsBundle\JbtronicsSettingsBundle::class => ['all' => true],
+    Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
 ];
