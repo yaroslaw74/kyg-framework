ec58d8b34f257ebab253b954db9b796b1923630e
Ярослав <yaroslaw74@gmail.com>
2026-06-15 22:49:45 +0300

    Update CoreController.php

--- src/Controller/CoreController.php
+++ src/Controller/CoreController.php
@@ -15,16 +15,14 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CoreController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
-    public function index(): Response
+    public function index(): RedirectResponse
     {
-        return $this->render('core/index.html.twig', [
-            'controller_name' => 'CoreController',
-        ]);
+        return $this->redirectToRoute('app');
     }
 }
