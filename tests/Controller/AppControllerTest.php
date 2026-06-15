de532a57b7b39929f6778e34eee0c8b2f5e57756
Ярослав <yaroslaw74@gmail.com>
2026-06-15 22:50:57 +0300

    Create AppControllerTest.php

---
+++ tests/Controller/AppControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Controller Test
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class AppControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = self::createClient();
+        $client->request('GET', '/app');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testHome(): void
+    {
+        $client = self::createClient();
+        $client->request('GET', '/app/home');
+
+        self::assertResponseIsSuccessful();
+    }
+}
