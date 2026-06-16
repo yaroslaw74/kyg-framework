989f8cfd3209a38530ebb750e1211bab29f3db61
Ярослав <yaroslaw74@gmail.com>
2026-06-16 21:16:12 +0300

    Update reference.php

--- config/reference.php
+++ config/reference.php
@@ -1771,6 +1771,9 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         ...<string, mixed>
  *     }>,
  * }
+ * @psalm-type SymfonycastsVerifyEmailConfig = array{
+ *     lifetime?: int|Param, // The length of time in seconds that a signed URI is valid for after it is created. // Default: 3600
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1796,6 +1799,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     nelmio_js_logger?: NelmioJsLoggerConfig,
  *     jbtronics_settings?: JbtronicsSettingsConfig,
  *     hwi_oauth?: HwiOauthConfig,
+ *     symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1825,6 +1829,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         nelmio_js_logger?: NelmioJsLoggerConfig,
  *         jbtronics_settings?: JbtronicsSettingsConfig,
  *         hwi_oauth?: HwiOauthConfig,
+ *         symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1852,6 +1857,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         nelmio_js_logger?: NelmioJsLoggerConfig,
  *         jbtronics_settings?: JbtronicsSettingsConfig,
  *         hwi_oauth?: HwiOauthConfig,
+ *         symfonycasts_verify_email?: SymfonycastsVerifyEmailConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
