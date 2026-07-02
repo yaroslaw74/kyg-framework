8e5f0505f8366e0059f95ad46e59235e41468719
Ярослав <yaroslaw74@gmail.com>
2026-07-02 20:21:28 +0300

    Update reference.php

--- config/reference.php
+++ config/reference.php
@@ -1818,6 +1818,28 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         locales?: array<string, scalar|Param|null>,
  *     },
  * }
+ * @psalm-type KnpPaginatorConfig = array{
+ *     default_options?: array{
+ *         sort_field_name?: scalar|Param|null, // Default: "sort"
+ *         sort_direction_name?: scalar|Param|null, // Default: "direction"
+ *         filter_field_name?: scalar|Param|null, // Default: "filterField"
+ *         filter_value_name?: scalar|Param|null, // Default: "filterValue"
+ *         page_name?: scalar|Param|null, // Default: "page"
+ *         distinct?: bool|Param, // Default: true
+ *         page_out_of_range?: scalar|Param|null, // Default: "ignore"
+ *         default_limit?: scalar|Param|null, // Default: 10
+ *     },
+ *     template?: array{
+ *         pagination?: scalar|Param|null, // Default: "@KnpPaginator/Pagination/sliding.html.twig"
+ *         rel_links?: scalar|Param|null, // Default: "@KnpPaginator/Pagination/rel_links.html.twig"
+ *         filtration?: scalar|Param|null, // Default: "@KnpPaginator/Pagination/filtration.html.twig"
+ *         sortable?: scalar|Param|null, // Default: "@KnpPaginator/Pagination/sortable_link.html.twig"
+ *     },
+ *     page_range?: scalar|Param|null, // Default: 5
+ *     page_limit?: scalar|Param|null, // Default: null
+ *     convert_exception?: bool|Param, // Default: false
+ *     remove_first_page_param?: bool|Param, // Default: false
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1848,6 +1870,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     pyrrah_gravatar?: PyrrahGravatarConfig,
  *     misd_phone_number?: MisdPhoneNumberConfig,
  *     sonata_intl?: SonataIntlConfig,
+ *     knp_paginator?: KnpPaginatorConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1882,6 +1905,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         pyrrah_gravatar?: PyrrahGravatarConfig,
  *         misd_phone_number?: MisdPhoneNumberConfig,
  *         sonata_intl?: SonataIntlConfig,
+ *         knp_paginator?: KnpPaginatorConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1914,6 +1938,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         pyrrah_gravatar?: PyrrahGravatarConfig,
  *         misd_phone_number?: MisdPhoneNumberConfig,
  *         sonata_intl?: SonataIntlConfig,
+ *         knp_paginator?: KnpPaginatorConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
