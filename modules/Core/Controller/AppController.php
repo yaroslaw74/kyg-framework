9edcd588d9be0892656f56bacca0ce8213b0cba6
Ярослав <yaroslaw74@gmail.com>
2026-07-26 20:23:37 +0300

    Update AppController.php

--- modules/Core/Controller/AppController.php
+++ modules/Core/Controller/AppController.php
@@ -19,6 +19,9 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * @see \App\Tests\Controller\AppControllerTest
+ */
 final class AppController extends AbstractController
 {
     #[Route('/app', name: 'app')]
