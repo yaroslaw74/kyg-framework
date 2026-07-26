a0d2bb97e20ec2bdd1553cfa46216fb25663e82b
Ярослав <yaroslaw74@gmail.com>
2026-07-26 21:31:54 +0300

    Create Status.php

---
+++ modules/Users/Enum/Status.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * KYG Framework for Business.
+ *
+ * @category   Enum
+ *
+ * @version    1.0.0
+ *
+ * @copyright  Copyright (c) Kataev Yaroslav
+ * @license    GNU General Public License version 3 or later, see LICENSE
+ */
+declare(strict_types=1);
+
+namespace App\Modules\Users\Enum;
+
+enum Status: string
+{
+    case New = 'New';
+    case Pending = 'Pending';
+    case Validated = 'Validated';
+    case Inactive = 'Inactive';
+    case Active = 'Active';
+    case Banned = 'Banned';
+    case Disabled = 'Disabled';
+    case Deleted = 'Deleted';
+}
