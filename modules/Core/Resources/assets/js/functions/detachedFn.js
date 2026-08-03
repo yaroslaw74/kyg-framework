b131285f2bbde10073539589ff5ace6ab72d20d8
Ярослав <yaroslaw74@gmail.com>
2026-08-03 20:04:51 +0300

    Delete detachedFn.js

--- modules/Core/Resources/assets/js/functions/detachedFn.js
+++
@@ -1,8 +0,0 @@
-import { toggleSidemenu } from "./toggleSidemenu.js";
-
-export function detachedFn() {
-    const HTML = document.querySelector("html");
-    HTML.setAttribute("data-nav-layout", "vertical");
-    HTML.setAttribute("data-vertical-style", "detached");
-    toggleSidemenu();
-}
